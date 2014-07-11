615e0a8b21a42e8dc18437ebd1f4c570534b3870
pplu <jlmartinez@capside.com>
2014-07-11 00:47:57 -0700

    Add new AWS services

---
+++ auto-lib/Aws/CloudWatchLogs.pm
@@ -0,0 +1,356 @@
+
+use AWS::API;
+
+
+package Aws::CloudWatchLogs::ExtractedValues {
+  use Moose;
+  with 'AWS::API::StrToStrMapParser';
+  has Map => (is => 'ro', isa => 'HashRef[Str]');
+}
+
+package Aws::CloudWatchLogs::InputLogEvent {
+  use Moose;
+  with ('AWS::API::ResultParser');
+  has message => (is => 'ro', isa => 'Str', required => 1);
+  has timestamp => (is => 'ro', isa => 'Num', required => 1);
+}
+
+package Aws::CloudWatchLogs::LogGroup {
+  use Moose;
+  with ('AWS::API::ResultParser');
+  has arn => (is => 'ro', isa => 'Str');
+  has creationTime => (is => 'ro', isa => 'Num');
+  has logGroupName => (is => 'ro', isa => 'Str');
+  has metricFilterCount => (is => 'ro', isa => 'Int');
+  has retentionInDays => (is => 'ro', isa => 'Int');
+  has storedBytes => (is => 'ro', isa => 'Num');
+}
+
+package Aws::CloudWatchLogs::LogStream {
+  use Moose;
+  with ('AWS::API::ResultParser');
+  has arn => (is => 'ro', isa => 'Str');
+  has creationTime => (is => 'ro', isa => 'Num');
+  has firstEventTimestamp => (is => 'ro', isa => 'Num');
+  has lastEventTimestamp => (is => 'ro', isa => 'Num');
+  has lastIngestionTime => (is => 'ro', isa => 'Num');
+  has logStreamName => (is => 'ro', isa => 'Str');
+  has storedBytes => (is => 'ro', isa => 'Num');
+  has uploadSequenceToken => (is => 'ro', isa => 'Str');
+}
+
+package Aws::CloudWatchLogs::MetricFilter {
+  use Moose;
+  with ('AWS::API::ResultParser');
+  has creationTime => (is => 'ro', isa => 'Num');
+  has filterName => (is => 'ro', isa => 'Str');
+  has filterPattern => (is => 'ro', isa => 'Str');
+  has metricTransformations => (is => 'ro', isa => 'ArrayRef[Aws::CloudWatchLogs::MetricTransformation]');
+}
+
+package Aws::CloudWatchLogs::MetricFilterMatchRecord {
+  use Moose;
+  with ('AWS::API::ResultParser');
+  has eventMessage => (is => 'ro', isa => 'Str');
+  has eventNumber => (is => 'ro', isa => 'Num');
+  has extractedValues => (is => 'ro', isa => 'Aws::CloudWatchLogs::ExtractedValues');
+}
+
+package Aws::CloudWatchLogs::MetricTransformation {
+  use Moose;
+  with ('AWS::API::ResultParser');
+  has metricName => (is => 'ro', isa => 'Str', required => 1);
+  has metricNamespace => (is => 'ro', isa => 'Str', required => 1);
+  has metricValue => (is => 'ro', isa => 'Str', required => 1);
+}
+
+package Aws::CloudWatchLogs::OutputLogEvent {
+  use Moose;
+  with ('AWS::API::ResultParser');
+  has ingestionTime => (is => 'ro', isa => 'Num');
+  has message => (is => 'ro', isa => 'Str');
+  has timestamp => (is => 'ro', isa => 'Num');
+}
+
+
+
+package Aws::CloudWatchLogs::CreateLogGroup {
+  use Moose;
+  has logGroupName => (is => 'ro', isa => 'Str', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'CreateLogGroup');
+  class_has _returns => (isa => 'Str', is => 'ro');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::CreateLogStream {
+  use Moose;
+  has logGroupName => (is => 'ro', isa => 'Str', required => 1);
+  has logStreamName => (is => 'ro', isa => 'Str', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'CreateLogStream');
+  class_has _returns => (isa => 'Str', is => 'ro');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::DeleteLogGroup {
+  use Moose;
+  has logGroupName => (is => 'ro', isa => 'Str', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'DeleteLogGroup');
+  class_has _returns => (isa => 'Str', is => 'ro');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::DeleteLogStream {
+  use Moose;
+  has logGroupName => (is => 'ro', isa => 'Str', required => 1);
+  has logStreamName => (is => 'ro', isa => 'Str', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'DeleteLogStream');
+  class_has _returns => (isa => 'Str', is => 'ro');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::DeleteMetricFilter {
+  use Moose;
+  has filterName => (is => 'ro', isa => 'Str', required => 1);
+  has logGroupName => (is => 'ro', isa => 'Str', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'DeleteMetricFilter');
+  class_has _returns => (isa => 'Str', is => 'ro');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::DeleteRetentionPolicy {
+  use Moose;
+  has logGroupName => (is => 'ro', isa => 'Str', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'DeleteRetentionPolicy');
+  class_has _returns => (isa => 'Str', is => 'ro');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::DescribeLogGroups {
+  use Moose;
+  has limit => (is => 'ro', isa => 'Int');
+  has logGroupNamePrefix => (is => 'ro', isa => 'Str');
+  has nextToken => (is => 'ro', isa => 'Str');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'DescribeLogGroups');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CloudWatchLogs::DescribeLogGroupsResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::DescribeLogStreams {
+  use Moose;
+  has limit => (is => 'ro', isa => 'Int');
+  has logGroupName => (is => 'ro', isa => 'Str', required => 1);
+  has logStreamNamePrefix => (is => 'ro', isa => 'Str');
+  has nextToken => (is => 'ro', isa => 'Str');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'DescribeLogStreams');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CloudWatchLogs::DescribeLogStreamsResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::DescribeMetricFilters {
+  use Moose;
+  has filterNamePrefix => (is => 'ro', isa => 'Str');
+  has limit => (is => 'ro', isa => 'Int');
+  has logGroupName => (is => 'ro', isa => 'Str', required => 1);
+  has nextToken => (is => 'ro', isa => 'Str');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'DescribeMetricFilters');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CloudWatchLogs::DescribeMetricFiltersResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::GetLogEvents {
+  use Moose;
+  has endTime => (is => 'ro', isa => 'Num');
+  has limit => (is => 'ro', isa => 'Int');
+  has logGroupName => (is => 'ro', isa => 'Str', required => 1);
+  has logStreamName => (is => 'ro', isa => 'Str', required => 1);
+  has nextToken => (is => 'ro', isa => 'Str');
+  has startFromHead => (is => 'ro', isa => 'Bool');
+  has startTime => (is => 'ro', isa => 'Num');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'GetLogEvents');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CloudWatchLogs::GetLogEventsResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::PutLogEvents {
+  use Moose;
+  has logEvents => (is => 'ro', isa => 'ArrayRef[Aws::CloudWatchLogs::InputLogEvent]', required => 1);
+  has logGroupName => (is => 'ro', isa => 'Str', required => 1);
+  has logStreamName => (is => 'ro', isa => 'Str', required => 1);
+  has sequenceToken => (is => 'ro', isa => 'Str');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'PutLogEvents');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CloudWatchLogs::PutLogEventsResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::PutMetricFilter {
+  use Moose;
+  has filterName => (is => 'ro', isa => 'Str', required => 1);
+  has filterPattern => (is => 'ro', isa => 'Str', required => 1);
+  has logGroupName => (is => 'ro', isa => 'Str', required => 1);
+  has metricTransformations => (is => 'ro', isa => 'ArrayRef[Aws::CloudWatchLogs::MetricTransformation]', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'PutMetricFilter');
+  class_has _returns => (isa => 'Str', is => 'ro');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::PutRetentionPolicy {
+  use Moose;
+  has logGroupName => (is => 'ro', isa => 'Str', required => 1);
+  has retentionInDays => (is => 'ro', isa => 'Int', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'PutRetentionPolicy');
+  class_has _returns => (isa => 'Str', is => 'ro');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CloudWatchLogs::TestMetricFilter {
+  use Moose;
+  has filterPattern => (is => 'ro', isa => 'Str', required => 1);
+  has logEventMessages => (is => 'ro', isa => 'ArrayRef[Str]', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'TestMetricFilter');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CloudWatchLogs::TestMetricFilterResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+
+package Aws::CloudWatchLogs::DescribeLogGroupsResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has logGroups => (is => 'ro', isa => 'ArrayRef[Aws::CloudWatchLogs::LogGroup]');
+  has nextToken => (is => 'ro', isa => 'Str');
+
+}
+package Aws::CloudWatchLogs::DescribeLogStreamsResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has logStreams => (is => 'ro', isa => 'ArrayRef[Aws::CloudWatchLogs::LogStream]');
+  has nextToken => (is => 'ro', isa => 'Str');
+
+}
+package Aws::CloudWatchLogs::DescribeMetricFiltersResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has metricFilters => (is => 'ro', isa => 'ArrayRef[Aws::CloudWatchLogs::MetricFilter]');
+  has nextToken => (is => 'ro', isa => 'Str');
+
+}
+package Aws::CloudWatchLogs::GetLogEventsResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has events => (is => 'ro', isa => 'ArrayRef[Aws::CloudWatchLogs::OutputLogEvent]');
+  has nextBackwardToken => (is => 'ro', isa => 'Str');
+  has nextForwardToken => (is => 'ro', isa => 'Str');
+
+}
+package Aws::CloudWatchLogs::PutLogEventsResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has nextSequenceToken => (is => 'ro', isa => 'Str');
+
+}
+package Aws::CloudWatchLogs::TestMetricFilterResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has matches => (is => 'ro', isa => 'ArrayRef[Aws::CloudWatchLogs::MetricFilterMatchRecord]');
+
+}
+
+package Aws::CloudWatchLogs {
+  use Moose;
+  has service => (is => 'ro', isa => 'Str', default => 'logs');
+  has version => (is => 'ro', isa => 'Str', default => '2014-03-28');
+  has target_prefix => (is => 'ro', isa => 'Str', default => 'Logs_20140328');
+  has json_version => (is => 'ro', isa => 'Str', default => "1.1");
+
+  use MooseX::ClassAttribute;
+  class_has endpoint_role => (is => 'ro', isa => 'Str', default => 'AWS::API::RegionalEndpointCaller');
+  class_has signature_role => (is => 'ro', isa => 'Str', default => 'Net::AWS::V4Signature');
+  class_has parameter_role => (is => 'ro', isa => 'Str', default => 'Net::AWS::JsonCaller');
+  class_has response_role => (is => 'ro', isa => 'Str', default => 'Net::AWS::JsonResponse');
+
+  
+  sub CreateLogGroup {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::CreateLogGroup', @_);
+  }
+  sub CreateLogStream {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::CreateLogStream', @_);
+  }
+  sub DeleteLogGroup {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::DeleteLogGroup', @_);
+  }
+  sub DeleteLogStream {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::DeleteLogStream', @_);
+  }
+  sub DeleteMetricFilter {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::DeleteMetricFilter', @_);
+  }
+  sub DeleteRetentionPolicy {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::DeleteRetentionPolicy', @_);
+  }
+  sub DescribeLogGroups {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::DescribeLogGroups', @_);
+  }
+  sub DescribeLogStreams {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::DescribeLogStreams', @_);
+  }
+  sub DescribeMetricFilters {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::DescribeMetricFilters', @_);
+  }
+  sub GetLogEvents {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::GetLogEvents', @_);
+  }
+  sub PutLogEvents {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::PutLogEvents', @_);
+  }
+  sub PutMetricFilter {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::PutMetricFilter', @_);
+  }
+  sub PutRetentionPolicy {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::PutRetentionPolicy', @_);
+  }
+  sub TestMetricFilter {
+    my $self = shift;
+    return $self->do_call('Aws::CloudWatchLogs::TestMetricFilter', @_);
+  }
+}
+1;
---
+++ auto-lib/Aws/CognitoIdentity.pm
@@ -0,0 +1,244 @@
+
+use AWS::API;
+
+
+package Aws::CognitoIdentity::IdentityDescription {
+  use Moose;
+  with ('AWS::API::ResultParser');
+  has IdentityId => (is => 'ro', isa => 'Str');
+  has Logins => (is => 'ro', isa => 'ArrayRef[Str]');
+}
+
+package Aws::CognitoIdentity::IdentityPoolShortDescription {
+  use Moose;
+  with ('AWS::API::ResultParser');
+  has IdentityPoolId => (is => 'ro', isa => 'Str');
+  has IdentityPoolName => (is => 'ro', isa => 'Str');
+}
+
+package Aws::CognitoIdentity::IdentityProviders {
+  use Moose;
+  with 'AWS::API::StrToStrMapParser';
+  has Map => (is => 'ro', isa => 'HashRef[Str]');
+}
+
+package Aws::CognitoIdentity::LoginsMap {
+  use Moose;
+  with 'AWS::API::StrToStrMapParser';
+  has Map => (is => 'ro', isa => 'HashRef[Str]');
+}
+
+
+
+package Aws::CognitoIdentity::CreateIdentityPool {
+  use Moose;
+  has AllowUnauthenticatedIdentities => (is => 'ro', isa => 'Bool', required => 1);
+  has IdentityPoolName => (is => 'ro', isa => 'Str', required => 1);
+  has SupportedLoginProviders => (is => 'ro', isa => 'Aws::CognitoIdentity::IdentityProviders');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'CreateIdentityPool');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CognitoIdentity::CreateIdentityPoolResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CognitoIdentity::DeleteIdentityPool {
+  use Moose;
+  has IdentityPoolId => (is => 'ro', isa => 'Str', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'DeleteIdentityPool');
+  class_has _returns => (isa => 'Str', is => 'ro');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CognitoIdentity::DescribeIdentityPool {
+  use Moose;
+  has IdentityPoolId => (is => 'ro', isa => 'Str', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'DescribeIdentityPool');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CognitoIdentity::DescribeIdentityPoolResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CognitoIdentity::GetId {
+  use Moose;
+  has AccountId => (is => 'ro', isa => 'Str', required => 1);
+  has IdentityPoolId => (is => 'ro', isa => 'Str', required => 1);
+  has Logins => (is => 'ro', isa => 'Aws::CognitoIdentity::LoginsMap');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'GetId');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CognitoIdentity::GetIdResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CognitoIdentity::GetOpenIdToken {
+  use Moose;
+  has IdentityId => (is => 'ro', isa => 'Str', required => 1);
+  has Logins => (is => 'ro', isa => 'Aws::CognitoIdentity::LoginsMap');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'GetOpenIdToken');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CognitoIdentity::GetOpenIdTokenResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CognitoIdentity::ListIdentities {
+  use Moose;
+  has IdentityPoolId => (is => 'ro', isa => 'Str', required => 1);
+  has MaxResults => (is => 'ro', isa => 'Int');
+  has NextToken => (is => 'ro', isa => 'Str');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'ListIdentities');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CognitoIdentity::ListIdentitiesResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CognitoIdentity::ListIdentityPools {
+  use Moose;
+  has MaxResults => (is => 'ro', isa => 'Int');
+  has NextToken => (is => 'ro', isa => 'Str');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'ListIdentityPools');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CognitoIdentity::ListIdentityPoolsResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CognitoIdentity::UnlinkIdentity {
+  use Moose;
+  has IdentityId => (is => 'ro', isa => 'Str', required => 1);
+  has Logins => (is => 'ro', isa => 'Aws::CognitoIdentity::LoginsMap', required => 1);
+  has LoginsToRemove => (is => 'ro', isa => 'ArrayRef[Str]', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'UnlinkIdentity');
+  class_has _returns => (isa => 'Str', is => 'ro');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+package Aws::CognitoIdentity::UpdateIdentityPool {
+  use Moose;
+  has AllowUnauthenticatedIdentities => (is => 'ro', isa => 'Bool', required => 1);
+  has IdentityPoolId => (is => 'ro', isa => 'Str', required => 1);
+  has IdentityPoolName => (is => 'ro', isa => 'Str', required => 1);
+  has SupportedLoginProviders => (is => 'ro', isa => 'Aws::CognitoIdentity::IdentityProviders');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'UpdateIdentityPool');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Aws::CognitoIdentity::UpdateIdentityPoolResult');
+  class_has _result_key => (isa => 'Str', is => 'ro');
+}
+
+package Aws::CognitoIdentity::CreateIdentityPoolResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has AllowUnauthenticatedIdentities => (is => 'ro', isa => 'Bool', required => 1);
+  has IdentityPoolId => (is => 'ro', isa => 'Str', required => 1);
+  has IdentityPoolName => (is => 'ro', isa => 'Str', required => 1);
+  has SupportedLoginProviders => (is => 'ro', isa => 'Aws::CognitoIdentity::IdentityProviders');
+
+}
+package Aws::CognitoIdentity::DescribeIdentityPoolResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has AllowUnauthenticatedIdentities => (is => 'ro', isa => 'Bool', required => 1);
+  has IdentityPoolId => (is => 'ro', isa => 'Str', required => 1);
+  has IdentityPoolName => (is => 'ro', isa => 'Str', required => 1);
+  has SupportedLoginProviders => (is => 'ro', isa => 'Aws::CognitoIdentity::IdentityProviders');
+
+}
+package Aws::CognitoIdentity::GetIdResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has IdentityId => (is => 'ro', isa => 'Str');
+
+}
+package Aws::CognitoIdentity::GetOpenIdTokenResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has IdentityId => (is => 'ro', isa => 'Str');
+  has Token => (is => 'ro', isa => 'Str');
+
+}
+package Aws::CognitoIdentity::ListIdentitiesResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has Identities => (is => 'ro', isa => 'ArrayRef[Aws::CognitoIdentity::IdentityDescription]');
+  has IdentityPoolId => (is => 'ro', isa => 'Str');
+  has NextToken => (is => 'ro', isa => 'Str');
+
+}
+package Aws::CognitoIdentity::ListIdentityPoolsResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has IdentityPools => (is => 'ro', isa => 'ArrayRef[Aws::CognitoIdentity::IdentityPoolShortDescription]');
+  has NextToken => (is => 'ro', isa => 'Str');
+
+}
+package Aws::CognitoIdentity::UpdateIdentityPoolResult {
+  use Moose;
+  with 'AWS::API::ResultParser';
+  has AllowUnauthenticatedIdentities => (is => 'ro', isa => 'Bool', required => 1);
+  has IdentityPoolId => (is => 'ro', isa => 'Str', required => 1);
+  has IdentityPoolName => (is => 'ro', isa => 'Str', required => 1);
+  has SupportedLoginProviders => (is => 'ro', isa => 'Aws::CognitoIdentity::IdentityProviders');
+
+}
+
+package Aws::CognitoIdentity {
+  use Moose;
+  has service => (is => 'ro', isa => 'Str', default => 'cognito-identity');
+  has version => (is => 'ro', isa => 'Str', default => '2014-06-30');
+  has target_prefix => (is => 'ro', isa => 'Str', default => 'AWSCognitoIdentityService');
+  has json_version => (is => 'ro', isa => 'Str', default => "1.1");
+
+  use MooseX::ClassAttribute;
+  class_has endpoint_role => (is => 'ro', isa => 'Str', default => 'AWS::API::RegionalEndpointCaller');
+  class_has signature_role => (is => 'ro', isa => 'Str', default => 'Net::AWS::V4Signature');
+  class_has parameter_role => (is => 'ro', isa => 'Str', default => 'Net::AWS::JsonCaller');
+  class_has response_role => (is => 'ro', isa => 'Str', default => 'Net::AWS::JsonResponse');
+
+  
+  sub CreateIdentityPool {
+    my $self = shift;
+    return $self->do_call('Aws::CognitoIdentity::CreateIdentityPool', @_);
+  }
+  sub DeleteIdentityPool {
+    my $self = shift;
+    return $self->do_call('Aws::CognitoIdentity::DeleteIdentityPool', @_);
+  }
+  sub DescribeIdentityPool {
+    my $self = shift;
+    return $self->do_call('Aws::CognitoIdentity::DescribeIdentityPool', @_);
+  }
+  sub GetId {
+    my $self = shift;
+    return $self->do_call('Aws::CognitoIdentity::GetId', @_);
+  }
+  sub GetOpenIdToken {
+    my $self = shift;
+    return $self->do_call('Aws::CognitoIdentity::GetOpenIdToken', @_);
+  }
+  sub ListIdentities {
+    my $self = shift;
+    return $self->do_call('Aws::CognitoIdentity::ListIdentities', @_);
+  }
+  sub ListIdentityPools {
+    my $self = shift;
+    return $self->do_call('Aws::CognitoIdentity::ListIdentityPools', @_);
+  }
+  sub UnlinkIdentity {
+    my $self = shift;
+    return $self->do_call('Aws::CognitoIdentity::UnlinkIdentity', @_);
+  }
+  sub UpdateIdentityPool {
+    my $self = shift;
+    return $self->do_call('Aws::CognitoIdentity::UpdateIdentityPool', @_);
+  }
+}
+1;
