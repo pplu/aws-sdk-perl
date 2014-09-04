edb13d3de26bac7c0d25c88eca808b886b017ecb
pplu <jlmartinez@capside.com>
2014-09-04 12:25:21 -0700

    IAM summaryMap type regenerated with patch to botocore

--- auto-lib/Aws/IAM/summaryMapType.pm
+++ auto-lib/Aws/IAM/summaryMapType.pm
@@ -1,14 +1,25 @@
 package Aws::IAM::summaryMapType {
   use Moose;
   with 'AWS::API::MapParser';
+
+  use MooseX::ClassAttribute;
+  class_has xml_keys   => (is => 'ro', isa => 'Str', default => 'key');
+  class_has xml_values => (is => 'ro', isa => 'Str', default => 'value');
+
   has AccessKeysPerUserQuota => (is => 'ro', isa => 'Int');
   has AccountMFAEnabled => (is => 'ro', isa => 'Int');
+  has AssumeRolePolicySizeQuota => (is => 'ro', isa => 'Int');
   has GroupPolicySizeQuota => (is => 'ro', isa => 'Int');
   has Groups => (is => 'ro', isa => 'Int');
   has GroupsPerUserQuota => (is => 'ro', isa => 'Int');
   has GroupsQuota => (is => 'ro', isa => 'Int');
+  has InstanceProfiles => (is => 'ro', isa => 'Int');
+  has InstanceProfilesQuota => (is => 'ro', isa => 'Int');
   has MFADevices => (is => 'ro', isa => 'Int');
   has MFADevicesInUse => (is => 'ro', isa => 'Int');
+  has RolePolicySizeQuota => (is => 'ro', isa => 'Int');
+  has Roles => (is => 'ro', isa => 'Int');
+  has RolesQuota => (is => 'ro', isa => 'Int');
   has ServerCertificates => (is => 'ro', isa => 'Int');
   has ServerCertificatesQuota => (is => 'ro', isa => 'Int');
   has SigningCertificatesPerUserQuota => (is => 'ro', isa => 'Int');
