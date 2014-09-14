7254304b240683b0555007b639bb5d591232e472
pplu <jlmartinez@capside.com>
2014-09-14 12:49:55 -0700

    Add classes generated by last update

--- auto-lib/Aws/EC2/DhcpConfiguration.pm
+++ auto-lib/Aws/EC2/DhcpConfiguration.pm
@@ -1,6 +1,6 @@
 package Aws::EC2::DhcpConfiguration {
   use Moose;
   has Key => (is => 'ro', isa => 'Str', traits => ['Unwrapped'], xmlname => 'key');
-  has Values => (is => 'ro', isa => 'ArrayRef[Defined]', traits => ['Unwrapped'], xmlname => 'valueSet');
+  has Values => (is => 'ro', isa => 'ArrayRef[Str]', traits => ['Unwrapped'], xmlname => 'valueSet');
 }
 1
