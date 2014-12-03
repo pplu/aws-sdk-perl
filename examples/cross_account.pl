e465df49a522b173cf9596f01d27d5049f937377
pplu <pplusdomain@gmail.com>
2014-12-03 23:58:47 +0100

    Implement a Credential Provider for CrossAccount Access
    Provide an example of use

---
+++ examples/cross_account.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use Paws;
+use Paws::Net::AssumeRoleCredentials;
+
+my $ec2 = Paws->service('EC2')->new(
+  region => 'eu-west-1', 
+  credentials => Paws::Net::AssumeRoleCredentials->new(
+    RoleArn => 'arn:aws:iam::123456789012:role/AdminCapside',
+    RoleSessionName => 'CrossAccountTest',
+    ExternalId => 'MyExternalId',
+  )
+);
+
+my $result = $ec2->DescribeSecurityGroups;
+
+use Data::Dumper;
+print Dumper($result);
