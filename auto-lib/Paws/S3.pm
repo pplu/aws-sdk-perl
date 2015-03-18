3ce16786162e0afd8e4afec7aa6759ed543eaea7
pplu <pplusdomain@gmail.com>
2015-03-18 22:34:22 +0100

    S3 docu update

--- auto-lib/Paws/S3.pm
+++ auto-lib/Paws/S3.pm
@@ -277,27 +277,6 @@ Paws::S3 - Perl Interface to AWS Amazon Simple Storage Service
 
 
 
-Amazon Simple Storage Service
-
-Amazon Simple Storage Service is storage for the Internet. It is
-designed to make web-scale computing easier for developers.
-
-Amazon S3 has a simple web services interface that you can use to store
-and retrieve any amount of data, at any time, from anywhere on the web.
-It gives any developer access to the same highly scalable, reliable,
-fast, inexpensive data storage infrastructure that Amazon uses to run
-its own global network of web sites. The service aims to maximize
-benefits of scale and to pass those benefits on to developers.
-
-
-
-
-
-
-
-
-
-
 =head1 METHODS
 
 =head2 AbortMultipartUpload()
