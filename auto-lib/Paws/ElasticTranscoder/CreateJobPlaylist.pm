4348997f5b47b8ffca6b93f6842a5889a82bb3fe
Jose Luis Martinez <jlmartinez@capside.com>
2015-04-10 22:00:41 +0200

    ElasticTranscoder API update

--- auto-lib/Paws/ElasticTranscoder/CreateJobPlaylist.pm
+++ auto-lib/Paws/ElasticTranscoder/CreateJobPlaylist.pm
@@ -4,5 +4,6 @@ package Paws::ElasticTranscoder::CreateJobPlaylist {
   has HlsContentProtection => (is => 'ro', isa => 'Paws::ElasticTranscoder::HlsContentProtection');
   has Name => (is => 'ro', isa => 'Str');
   has OutputKeys => (is => 'ro', isa => 'ArrayRef[Str]');
+  has PlayReadyDrm => (is => 'ro', isa => 'Paws::ElasticTranscoder::PlayReadyDrm');
 }
 1;
--- auto-lib/Paws/ElasticTranscoder/Playlist.pm
+++ auto-lib/Paws/ElasticTranscoder/Playlist.pm
@@ -4,6 +4,7 @@ package Paws::ElasticTranscoder::Playlist {
   has HlsContentProtection => (is => 'ro', isa => 'Paws::ElasticTranscoder::HlsContentProtection');
   has Name => (is => 'ro', isa => 'Str');
   has OutputKeys => (is => 'ro', isa => 'ArrayRef[Str]');
+  has PlayReadyDrm => (is => 'ro', isa => 'Paws::ElasticTranscoder::PlayReadyDrm');
   has Status => (is => 'ro', isa => 'Str');
   has StatusDetail => (is => 'ro', isa => 'Str');
 }
