78b7ffd5ff97d02f94ad52f70345f4a863ba1566
Jose Luis Martinez <jlmartinez@capside.com>
2015-06-03 19:42:45 +0000

    Remove duplicates from AttributeMap

--- auto-lib/Paws/SQS/AttributeMap.pm
+++ auto-lib/Paws/SQS/AttributeMap.pm
@@ -7,10 +7,8 @@ package Paws::SQS::AttributeMap {
   class_has xml_values =>(is => 'ro', default => 'Value');
 
   has ApproximateFirstReceiveTimestamp => (is => 'ro', isa => 'Str');
-  has ApproximateFirstReceiveTimestamp => (is => 'ro', isa => 'Str');
   has ApproximateReceiveCount => (is => 'ro', isa => 'Str');
   has SenderId => (is => 'ro', isa => 'Str');
-  has SentTimestamp => (is => 'ro', isa => 'Str');
   has SentTimestamp => (is => 'ro', isa => 'Str');
 }
 1
