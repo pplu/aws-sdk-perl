2e59eadbef5ec2e7c9d8945a5f480d1d39437b13
Jose Luis Martinez <jlmartinez@capside.com>
2015-06-03 18:27:05 +0000

    Correct attribute definition for message attribute map

--- auto-lib/Paws/SQS/AttributeMap.pm
+++ auto-lib/Paws/SQS/AttributeMap.pm
@@ -6,18 +6,11 @@ package Paws::SQS::AttributeMap {
   class_has xml_keys =>(is => 'ro', default => 'Name');
   class_has xml_values =>(is => 'ro', default => 'Value');
 
-  has ApproximateNumberOfMessages => (is => 'ro', isa => 'Str');
-  has ApproximateNumberOfMessagesDelayed => (is => 'ro', isa => 'Str');
-  has ApproximateNumberOfMessagesNotVisible => (is => 'ro', isa => 'Str');
-  has CreatedTimestamp => (is => 'ro', isa => 'Str');
-  has DelaySeconds => (is => 'ro', isa => 'Str');
-  has LastModifiedTimestamp => (is => 'ro', isa => 'Str');
-  has MaximumMessageSize => (is => 'ro', isa => 'Str');
-  has MessageRetentionPeriod => (is => 'ro', isa => 'Str');
-  has Policy => (is => 'ro', isa => 'Str');
-  has QueueArn => (is => 'ro', isa => 'Str');
-  has ReceiveMessageWaitTimeSeconds => (is => 'ro', isa => 'Str');
-  has RedrivePolicy => (is => 'ro', isa => 'Str');
-  has VisibilityTimeout => (is => 'ro', isa => 'Str');
+  has ApproximateFirstReceiveTimestamp => (is => 'ro', isa => 'Str');
+  has ApproximateFirstReceiveTimestamp => (is => 'ro', isa => 'Str');
+  has ApproximateReceiveCount => (is => 'ro', isa => 'Str');
+  has SenderId => (is => 'ro', isa => 'Str');
+  has SentTimestamp => (is => 'ro', isa => 'Str');
+  has SentTimestamp => (is => 'ro', isa => 'Str');
 }
 1
