6f81c9f901cca3b4a857fb0726507e921bda5447
Jose Luis Martinez <jlmartinez@capside.com>
2015-06-21 22:40:03 +0200

    No use for SingleEndpointCaller, since the rule processing engine copes with everything

--- builder-lib/Paws/API/Builder.pm
+++ builder-lib/Paws/API/Builder.pm
@@ -19,10 +19,7 @@ package Paws::API::Builder {
 
   has service => (is => 'ro', lazy => 1, default => sub { $_[0]->api_struct->{metadata}->{ endpointPrefix } });
   has version => (is => 'ro', lazy => 1, default => sub { $_[0]->api_struct->{metadata}->{ apiVersion } });
-  has endpoint_role => (is => 'ro', lazy => 1, default => sub { defined $_[0]->api_struct->{metadata}->{ globalEndpoint } ? 
-                                                                   'Paws::API::SingleEndpointCaller':
-                                                                   'Paws::API::RegionalEndpointCaller' 
-                                                              } );
+  has endpoint_role => (is => 'ro', lazy => 1, default => 'Paws::API::RegionalEndpointCaller' );
 
   has api_file => (is => 'ro', required => 1);
 
