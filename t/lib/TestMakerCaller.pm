7fbef0560fc8cc3764247e12cf05f67df49ed4d0
Jose Luis Martinez <jlmartinez@capside.com>
2015-07-24 22:44:37 +0200

    Cleanup TestMakerCaller

--- t/lib/TestMakerCaller.pm
+++ t/lib/TestMakerCaller.pm
@@ -11,22 +11,9 @@ package TestMakerCaller {
 
     my $requestObj = $service->prepare_request_for_call($call_object); 
 
-use Data::Dumper;
-print Dumper($requestObj);
-
     my $headers = $requestObj->header_hash;
     # HTTP::Tiny has made setting Host header illegal. It derives Host from URL
     delete $headers->{Host};
-
-    my $url = $requestObj->url;
-    if ($requestObj->method eq 'GET') {
-      my @param;
-      for my $p (keys %{ $requestObj->parameters }) {
-        push @param , join '=' , map { $self->_uri_escape($_,"^A-Za-z0-9\-_.~") } ($p, $requestObj->parameters->{$p});
-      }
-      $url .= '?' . (join '&', @param) if (@param);
-      $requestObj->url($url);
-    }
 
     my $response = $self->ua->request(
       $requestObj->method,
