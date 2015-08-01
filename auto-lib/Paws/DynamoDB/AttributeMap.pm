b0fdaa458601f64dbc470b85c7185fc771328473
Jose Luis Martinez <jlmartinez@capside.com>
2015-08-01 11:53:11 +0200

    AttributeMap generated with update to botocore

--- auto-lib/Paws/DynamoDB/AttributeMap.pm
+++ auto-lib/Paws/DynamoDB/AttributeMap.pm
@@ -3,7 +3,7 @@ package Paws::DynamoDB::AttributeMap {
   with 'Paws::API::StrToObjMapParser';
 
   use MooseX::ClassAttribute;
-  class_has xml_keys =>(is => 'ro', default => 'key');
+  class_has xml_keys =>(is => 'ro', default => 'name');
   class_has xml_values =>(is => 'ro', default => 'value');
 
   has Map => (is => 'ro', isa => 'HashRef[Paws::DynamoDB::AttributeValue]');
