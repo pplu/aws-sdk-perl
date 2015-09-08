a40575b263b5836892e6d485aa3a7f504f2470b3
Jose Luis Martinez <jlmartinez@capside.com>
2015-09-08 22:42:35 +0200

    More DynamoDB tests + script to play with Dynamo

--- examples/dynamodb.pl
+++ examples/dynamodb.pl
@@ -1,32 +1,69 @@
 #!/usr/bin/env perl
 
+use strict;
+use warnings;
 use Paws;
 
 use Data::Printer;
 
-my $d = Paws->service('DynamoDB', region => 'eu-west-1' );
+my $table_name = 'Emails';
+
+my $d = Paws->service('DynamoDB', 
+  region => 'eu-west-1', 
+#  caller => TestMakerCaller->new,
+);
 my $r;
 
 $r = $d->ListTables;
 p $r;
 
-$r = $d->CreateTable(
-  AttributeDefinitions => [ 
-    { AttributeName => 'email', AttributeType => 'S' },
-    { AttributeName => 'count', AttributeType => 'N' },
-#    { AttributeName => 'number', AttributeType => 'N' },
-  ],
-  KeySchema => [
-    { AttributeName => 'email', KeyType => 'HASH' },
-    { AttributeName => 'count', KeyType => 'RANGE' },
-  ],
-  ProvisionedThroughput => {
-    ReadCapacityUnits => 1,
-    WriteCapacityUnits => 1
-  },
-  TableName => 'Emails',
+if (not grep { $_ eq $table_name } @{ $r->TableNames }){
+  $r = $d->CreateTable(
+    AttributeDefinitions => [ 
+      { AttributeName => 'email', AttributeType => 'S' },
+      { AttributeName => 'count', AttributeType => 'N' },
+  #    { AttributeName => 'number', AttributeType => 'N' },
+    ],
+    KeySchema => [
+      { AttributeName => 'email', KeyType => 'HASH' },
+      { AttributeName => 'count', KeyType => 'RANGE' },
+    ],
+    ProvisionedThroughput => {
+      ReadCapacityUnits => 1,
+      WriteCapacityUnits => 1
+    },
+    TableName => $table_name,
+  );
+  p $r;
+
+  # Let dynamodb create the table...
+  sleep 15;
+}
+
+$r = $d->Scan(TableName => $table_name);
+
+$d->PutItem(
+  TableName => $table_name,
+  Item => {
+    email => { S => 'e1@test.com' },
+    count => { N => '33' },
+  }
 );
 
-p $r;
+$r = $d->Scan(TableName => $table_name);
 
-$d->DeleteTable(TableName => 'Emails');
+my $i = $r->Items->[0];
+p $i;
+
+$r = $d->GetItem(
+  TableName => $table_name,
+  ConsistentRead => 1,
+  Key => {
+    email => { S => 'e1@test.com' },
+    count => { N => '33' },
+  }
+);
+
+p $r->Item;
+
+$d->DeleteTable(TableName => $table_name);
