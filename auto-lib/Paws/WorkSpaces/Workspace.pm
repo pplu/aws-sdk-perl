b7765d1080320c2887460d27a370db9130116667
Jose Luis Martinez <jlmartinez@capside.com>
2015-10-02 22:58:58 +0200

    Workspaces API update: Volume encrytption

--- auto-lib/Paws/WorkSpaces/Workspace.pm
+++ auto-lib/Paws/WorkSpaces/Workspace.pm
@@ -1,12 +1,16 @@
 package Paws::WorkSpaces::Workspace;
   use Moose;
   has BundleId => (is => 'ro', isa => 'Str');
+  has ComputerName => (is => 'ro', isa => 'Str');
   has DirectoryId => (is => 'ro', isa => 'Str');
   has ErrorCode => (is => 'ro', isa => 'Str');
   has ErrorMessage => (is => 'ro', isa => 'Str');
   has IpAddress => (is => 'ro', isa => 'Str');
+  has RootVolumeEncryptionEnabled => (is => 'ro', isa => 'Bool');
   has State => (is => 'ro', isa => 'Str');
   has SubnetId => (is => 'ro', isa => 'Str');
   has UserName => (is => 'ro', isa => 'Str');
+  has UserVolumeEncryptionEnabled => (is => 'ro', isa => 'Bool');
+  has VolumeEncryptionKey => (is => 'ro', isa => 'Str');
   has WorkspaceId => (is => 'ro', isa => 'Str');
 1;
--- auto-lib/Paws/WorkSpaces/WorkspaceRequest.pm
+++ auto-lib/Paws/WorkSpaces/WorkspaceRequest.pm
@@ -2,5 +2,8 @@ package Paws::WorkSpaces::WorkspaceRequest;
   use Moose;
   has BundleId => (is => 'ro', isa => 'Str', required => 1);
   has DirectoryId => (is => 'ro', isa => 'Str', required => 1);
+  has RootVolumeEncryptionEnabled => (is => 'ro', isa => 'Bool');
   has UserName => (is => 'ro', isa => 'Str', required => 1);
+  has UserVolumeEncryptionEnabled => (is => 'ro', isa => 'Bool');
+  has VolumeEncryptionKey => (is => 'ro', isa => 'Str');
 1;
