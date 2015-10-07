ce13de4e6a24088f8ff510615cd319844df054d5
Jose Luis Martinez <jlmartinez@capside.com>
2015-10-07 23:33:09 +0000

    New MarketplaceCommerceAnalytics API

---
+++ auto-lib/Paws/MarketplaceCommerceAnalytics/GenerateDataSetResult.pm
@@ -0,0 +1,22 @@
+
+package Paws::MarketplaceCommerceAnalytics::GenerateDataSetResult;
+  use Moose;
+  has dataSetRequestId => (is => 'ro', isa => 'Str');
+
+
+### main pod documentation begin ###
+
+=head1 NAME
+
+Paws::MarketplaceCommerceAnalytics::GenerateDataSetResult
+
+=head1 ATTRIBUTES
+
+=head2 dataSetRequestId => Str
+
+  
+
+
+=cut
+
+1;
