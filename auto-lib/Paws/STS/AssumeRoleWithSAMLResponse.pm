b0ee76842b0d24b4a80ba313e2d5b6710d1ecae2
Jose Luis Martinez <jlmartinez@capside.com>
2016-01-21 00:31:25 +0100

    STS docu update

--- auto-lib/Paws/STS/AssumeRoleWithSAMLResponse.pm
+++ auto-lib/Paws/STS/AssumeRoleWithSAMLResponse.pm
@@ -36,7 +36,14 @@ C<SubjectConfirmationData> element of the SAML assertion.
 
 =head2 Credentials => L<Paws::STS::Credentials>
 
+The temporary security credentials, which include an access key ID, a
+secret access key, and a security (or session) token.
 
+B<Note:> The size of the security token that STS APIs return is not
+fixed. We strongly recommend that you make no assumptions about the
+maximum size. As of this writing, the typical size is less than 4096
+bytes, but that can vary. Also, future updates to AWS might require
+larger sizes.
 
 
 
