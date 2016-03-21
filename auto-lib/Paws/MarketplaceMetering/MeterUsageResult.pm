e171b1fe31b7d138ab6882f90447b8bad4914e1d
Jose Luis Martinez <jlmartinez@capside.com>
2016-03-21 22:07:07 +0100

    New service: Marketplace Metering

---
+++ auto-lib/Paws/MarketplaceMetering/MeterUsageResult.pm
@@ -0,0 +1,25 @@
+
+package Paws::MarketplaceMetering::MeterUsageResult;
+  use Moose;
+  has MeteringRecordId => (is => 'ro', isa => 'Str');
+
+
+### main pod documentation begin ###
+
+=head1 NAME
+
+Paws::MarketplaceMetering::MeterUsageResult
+
+=head1 ATTRIBUTES
+
+
+=head2 MeteringRecordId => Str
+
+
+
+
+
+
+=cut
+
+1;
