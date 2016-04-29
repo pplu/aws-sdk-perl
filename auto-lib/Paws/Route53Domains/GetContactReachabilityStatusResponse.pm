91fc423a23339c3f02c8f569c2d7189047917b39
Jose Luis Martinez <jlmartinez@capside.com>
2016-04-29 21:32:50 +0200

    Route53Domains API update: ReachabilityEmail operations

---
+++ auto-lib/Paws/Route53Domains/GetContactReachabilityStatusResponse.pm
@@ -0,0 +1,40 @@
+
+package Paws::Route53Domains::GetContactReachabilityStatusResponse;
+  use Moose;
+  has DomainName => (is => 'ro', isa => 'Str', traits => ['Unwrapped'], xmlname => 'domainName' );
+  has Status => (is => 'ro', isa => 'Str', traits => ['Unwrapped'], xmlname => 'status' );
+
+
+### main pod documentation begin ###
+
+=head1 NAME
+
+Paws::Route53Domains::GetContactReachabilityStatusResponse
+
+=head1 ATTRIBUTES
+
+
+=head2 DomainName => Str
+
+The domain name for which you requested the reachability status.
+
+
+
+=head2 Status => Str
+
+Whether the registrant contact has responded. C<PENDING> indicates that
+we sent the confirmation email and haven't received a response yet,
+C<DONE> indicates that we sent the email and got confirmation from the
+registrant contact, and C<EXPIRED> indicates that the time limit
+expired before the registrant contact responded.
+
+Type: String
+
+Valid values: C<PENDING>, C<DONE>, C<EXPIRED>
+
+Valid values are: C<"PENDING">, C<"DONE">, C<"EXPIRED">
+
+
+=cut
+
+1;
