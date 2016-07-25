c3c1ec277d45743d9c0afdc7270f5a649d8b5dbe
Jose Luis Martinez <jlmartinez@capside.com>
2016-07-25 23:28:30 +0200

    Bump up version to 0.27

--- auto-lib/Paws.pm
+++ auto-lib/Paws.pm
@@ -57,7 +57,7 @@ __PACKAGE__->meta->make_immutable;
 
 package Paws;
 
-our $VERSION = '0.26';
+our $VERSION = '0.27';
 
 use Moose;
 use MooseX::ClassAttribute;
--- builder-lib/Paws/API/Builder/Paws.pm
+++ builder-lib/Paws/API/Builder/Paws.pm
@@ -9,7 +9,7 @@ package Paws::API::Builder::Paws {
   use Moose;
 
   sub version {
-    '0.26';
+    '0.27';
   }
 
   sub services {
