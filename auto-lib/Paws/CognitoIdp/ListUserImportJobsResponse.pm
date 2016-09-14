7905ef1338d782805488f0098b3cfb0e02d32b53
Jose Luis Martinez <jlmartinez@capside.com>
2016-09-14 23:01:49 +0200

    CognitoIdp API update: UserImportJob operations

---
+++ auto-lib/Paws/CognitoIdp/ListUserImportJobsResponse.pm
@@ -0,0 +1,33 @@
+
+package Paws::CognitoIdp::ListUserImportJobsResponse;
+  use Moose;
+  has PaginationToken => (is => 'ro', isa => 'Str');
+  has UserImportJobs => (is => 'ro', isa => 'ArrayRef[Paws::CognitoIdp::UserImportJobType]');
+
+
+### main pod documentation begin ###
+
+=head1 NAME
+
+Paws::CognitoIdp::ListUserImportJobsResponse
+
+=head1 ATTRIBUTES
+
+
+=head2 PaginationToken => Str
+
+An identifier that can be used to return the next set of user import
+jobs in the list.
+
+
+
+=head2 UserImportJobs => ArrayRef[L<Paws::CognitoIdp::UserImportJobType>]
+
+The user import jobs.
+
+
+
+
+=cut
+
+1;
