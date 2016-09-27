7516b630965202841e9d0c36d795da26a2626b02
Jose Luis Martinez <jlmartinez@capside.com>
2016-09-28 00:29:34 +0200

    Test XML-Returning with synthetic services and responses

--- t/lib/JsonParamsService.pm
+++ t/lib/JsonParamsService.pm
@@ -84,5 +84,56 @@ package Paws::JsonParamsService;
     my $call_object = $self->new_with_coercions('Paws::JsonParamsService::Method1', @_);
     return $self->caller->do_call($self, $call_object);
   }
+1;
+package Paws::JsonParamsService::Method2;
+  use Moose;
 
+  has response => (isa => 'Str', is => 'ro', required => 1);
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'Method2Result');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Paws::JsonParamsService::Method1Return');
+  class_has _result_key => (isa => 'Str', is => 'ro', default => 'Method2Result');
+1;
+package Paws::QueryFlattenedParamsService;
+  use Moose;
+  sub service { 'queryflattenedparams' }
+  sub version { '2016-09-25' }
+  sub flattened_arrays { 1 }
+  has max_attempts => (is => 'ro', isa => 'Int', default => 5);
+  has retry => (is => 'ro', isa => 'HashRef', default => sub {
+    { base => 'rand', type => 'exponential', growth_factor => 2 }
+  });
+  has retriables => (is => 'ro', isa => 'ArrayRef', default => sub { [
+       sub { defined $_[0]->http_status and $_[0]->http_status == 403 and $_[0]->code eq 'RequestThrottled' },
+  ] });
+
+  with 'Paws::API::Caller', 'Paws::API::EndpointResolver', 'Paws::Net::V4Signature', 'Paws::Net::QueryCaller', 'Paws::Net::XMLResponse';
+
+  sub Method2 {
+    my $self = shift;
+    my $call_object = $self->new_with_coercions('Paws::JsonParamsService::Method2', @_);
+    return $self->caller->do_call($self, $call_object);
+  }
+1;
+package Paws::QueryParamsService;
+  use Moose;
+  sub service { 'queryparams' }
+  sub version { '2010-03-31' }
+  sub flattened_arrays { 0 }
+  has max_attempts => (is => 'ro', isa => 'Int', default => 5);
+  has retry => (is => 'ro', isa => 'HashRef', default => sub {
+    { base => 'rand', type => 'exponential', growth_factor => 2 }
+  });
+  has retriables => (is => 'ro', isa => 'ArrayRef', default => sub { [
+  ] });
+
+  with 'Paws::API::Caller', 'Paws::API::EndpointResolver', 'Paws::Net::V4Signature', 'Paws::Net::QueryCaller', 'Paws::Net::XMLResponse';
+
+  sub Method2 {
+    my $self = shift;
+    my $call_object = $self->new_with_coercions('Paws::JsonParamsService::Method2', @_);
+    return $self->caller->do_call($self, $call_object);
+  }
 1;
