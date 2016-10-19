526679a51f2808211478a3262d7ef8e3c877d094
Jose Luis Martinez <jlmartinez@capside.com>
2016-10-19 23:44:25 +0200

    ACM API update: ImportCertificate

---
+++ auto-lib/Paws/ACM/ImportCertificateResponse.pm
@@ -0,0 +1,25 @@
+
+package Paws::ACM::ImportCertificateResponse;
+  use Moose;
+  has CertificateArn => (is => 'ro', isa => 'Str');
+
+
+### main pod documentation begin ###
+
+=head1 NAME
+
+Paws::ACM::ImportCertificateResponse
+
+=head1 ATTRIBUTES
+
+
+=head2 CertificateArn => Str
+
+The Amazon Resource Name (ARN) of the imported certificate.
+
+
+
+
+=cut
+
+1;
