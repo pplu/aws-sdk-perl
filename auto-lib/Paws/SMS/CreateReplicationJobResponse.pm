ced5e3ed1a2abcb82706d0afdbd88fd5532b49ad
Jose Luis Martinez <jlmartinez@capside.com>
2016-10-25 22:10:36 +0200

    New API: SMS (Server Migration Service)

---
+++ auto-lib/Paws/SMS/CreateReplicationJobResponse.pm
@@ -0,0 +1,25 @@
+
+package Paws::SMS::CreateReplicationJobResponse;
+  use Moose;
+  has ReplicationJobId => (is => 'ro', isa => 'Str', traits => ['Unwrapped'], xmlname => 'replicationJobId' );
+
+
+### main pod documentation begin ###
+
+=head1 NAME
+
+Paws::SMS::CreateReplicationJobResponse
+
+=head1 ATTRIBUTES
+
+
+=head2 ReplicationJobId => Str
+
+
+
+
+
+
+=cut
+
+1;
---
+++ auto-lib/Paws/SMS/ImportServerCatalogResponse.pm
@@ -0,0 +1,18 @@
+
+package Paws::SMS::ImportServerCatalogResponse;
+  use Moose;
+
+
+### main pod documentation begin ###
+
+=head1 NAME
+
+Paws::SMS::ImportServerCatalogResponse
+
+=head1 ATTRIBUTES
+
+
+
+=cut
+
+1;
