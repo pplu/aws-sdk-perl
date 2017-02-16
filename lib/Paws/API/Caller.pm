6d107e1875f8a41c9df92faf43ca9b3a7796e5d9
Jose Luis Martinez <jlmartinez@capside.com>
2017-02-16 23:15:03 +0100

    move method that treats responses and converts them to objects to each Response role

--- lib/Paws/API/Caller.pm
+++ lib/Paws/API/Caller.pm
@@ -12,6 +12,8 @@ package Paws::API::Caller;
     handles => [ 'access_key', 'secret_key', 'session_token' ],
   );
 
+  requires 'new_from_result_struct';
+
   # converts the params the user passed to the call into objects that represent the call
   sub new_with_coercions {
     my ($self, $class, %params) = @_;
@@ -110,160 +112,11 @@ package Paws::API::Caller;
       $unserialized_struct->{ _request_id } = $request_id;
       
       Paws->load_class($call_object->_returns);
-      my $o_result = $self->new_from_struct($call_object->_returns, $unserialized_struct);
+      my $o_result = $self->new_from_result_struct($call_object->_returns, $unserialized_struct);
       return $o_result;
     } else {
       return 1;
     }
   }
 
-  sub new_from_struct {
-    my ($self, $class, $result) = @_;
-    my %args;
-    
-    if ($class->does('Paws::API::StrToObjMapParser')) {
-      return $self->handle_response_strtoobjmap($class, $result);
-    } elsif ($class->does('Paws::API::StrToNativeMapParser')) {
-      return $self->handle_response_strtonativemap($class, $result);
-    } else {
-    foreach my $att ($class->meta->get_attribute_list) {
-      next if (not my $meta = $class->meta->get_attribute($att));
-
-      my $key = $meta->does('Paws::API::Attribute::Trait::Unwrapped') ? $meta->xmlname :
-                $meta->does('Paws::API::Attribute::Trait::ParamInHeader') ? lc($meta->header_name) : $att;
-
-      my $att_type = $meta->type_constraint;
-
-    #  use Data::Dumper;
-    #  print STDERR "USING KEY:  $key\n";
-    #  print STDERR "$att IS A '$att_type' TYPE\n";
-    #  print STDERR "VALUE: " . Dumper($result);
-    #  my $extracted_val = $result->{ $key };
-    #  print STDERR "RESULT >>> $extracted_val\n";
-
-      # We'll consider that an attribute without brackets [] isn't an array type
-      if ($att_type !~ m/\[.*\]$/) {
-        my $value = $result->{ $key };
-        my $value_ref = ref($value);
-
-        if ($att_type =~ m/\:\:/) {
-          # Make the att_type stringify for module loading
-          Paws->load_class("$att_type");
-          if (defined $value) {
-            if (not $value_ref) {
-              $args{ $att } = $value;
-            } else {
-              my $att_class = $att_type->class;
-
-              if ($att_class->does('Paws::API::StrToObjMapParser')) {
-                $args{ $att } = $self->handle_response_strtoobjmap($att_class, $value);
-              } elsif ($att_class->does('Paws::API::StrToNativeMapParser')) {
-                $args{ $att } = $self->handle_response_strtonativemap($att_class, $value);
-              } elsif ($att_class->does('Paws::API::MapParser')) {
-                my $xml_keys = $att_class->xml_keys;
-                my $xml_values = $att_class->xml_values;
-
-                #TODO: handle in one place
-                if ($value_ref eq 'HASH') {
-                  if (exists $value->{ member }) {
-                    $value = $value->{ member };
-                  } elsif (exists $value->{ entry }) {
-                    $value = $value->{ entry  };
-                  } elsif (keys %$value == 1) {
-                    $value = $value->{ (keys %$value)[0] };
-                  } else {
-                    # Force it to be an arrayref and hope it is processed correctly
-                    $value = [ $value ];
-                  }
-                  $value_ref = ref($value);
-                }
-
-
-                $args{ $att } = $att_class->new(map { ($_->{ $xml_keys } => $_->{ $xml_values }) } @$value);
-              } else {
-                $args{ $att } = $self->new_from_struct($att_class, $value);
-              }
-            }
-          } else {
-              ##########
-              # This loop is required to guard against cases (such as Paws::S3::CopyObject) where
-              # the root node is removed from the response when unserialising (see KeepRoot => 1 for 
-              # XML::Simple) but is required to create the Paws object. This is mostly due to the 
-              # implementation of the new_from_struct sub 
-              my $att_class = $att_type->class;
-              eval {
-                $args{ $att } = $self->new_from_struct($att_class, $result);
-                1;
-              } or do {}
-          }
-        } else {
-          if (defined $value) {
-            if ($att_type eq 'Bool') {
-              if ($value eq 'true') {
-                $args{ $att } = 1;
-              } elsif ($value eq 'false') {
-                $args{ $att } = 0;
-              } elsif ($value == 1) {
-                $args{ $att } = 1;
-              } else {
-                $args{ $att } = 0;
-              }
-            } else {
-              $args{ $att } = $value;
-            }
-          }
-        }
-      } elsif (my ($type) = ($att_type =~ m/^ArrayRef\[(.*)\]$/)) {
-        my $value = $result->{ $att };
-        $value = $result->{ $key } if (not defined $value and $key ne $att);
-        my $value_ref = ref($value);
-
-        if ($value_ref eq 'HASH') {
-          if (exists $value->{ member }) {
-            $value = $value->{ member };
-          } elsif (exists $value->{ entry }) {
-            $value = $value->{ entry  };
-          } elsif (keys %$value == 1) {
-            $value = $value->{ (keys %$value)[0] };
-          } else {
-            #die "Can't detect the item that has the array in the response hash";
-          }
-          $value_ref = ref($value);
-        }
- 
-        if ($type =~ m/\:\:/) {
-          Paws->load_class($type);
-
-          my $val;
-          if (not defined $value) {
-            $val = [ ];
-          } elsif ($value_ref eq 'ARRAY') {
-            $val = $value;
-          } elsif ($value_ref eq 'HASH') {
-            $val = [ $value ];
-          }
-
-          if ($type->does('Paws::API::StrToObjMapParser')) {
-            $args{ $att } = [ map { $self->handle_response_strtoobjmap($type, $_) } @$val ];
-          } elsif ($type->does('Paws::API::StrToNativeMapParser')) {
-            $args{ $att } = [ map { $self->handle_response_strtonativemap($type, $_) } @$val ];
-          } elsif ($type->does('Paws::API::MapParser')) {
-            die "MapParser Type in an Array. Please implement me";
-          } else {
-            $args{ $att } = [ map { $self->new_from_struct($type, $_) } @$val ];
-          }
-        } else {
-          if (defined $value){
-            if ($value_ref eq 'ARRAY') {
-              $args{ $att } = $value; 
-            } else {
-              $args{ $att } = [ $value ];
-            }
-          }
-        }
-      }
-    }
-    $class->new(%args);
-    }
-  }
 1;
