82fd1e2939a512811317f8fe89522c198ecbd8dd
Jose Luis Martinez <jlmartinez@capside.com>
2017-05-26 14:36:40 +0000

    Move preloading endpoint and region_for_signature only to where needed
    The Signin service was not loading correctly (since it doesn't require
    that information)

--- lib/Paws/API/EndpointResolver.pm
+++ lib/Paws/API/EndpointResolver.pm
@@ -5,23 +5,6 @@ package Paws::API::EndpointResolver;
   use Paws::Exception;
   use Moose::Util::TypeConstraints;
 
-  sub BUILD {
-    my $self = shift;
-
-    # These calls are here so that when you construct
-    # the object the endpoint information and the _region_for_signature
-    # are calculated during construction. This is to avoid the fact that 
-    # these attributes are lazy (because they depend on other attributes) 
-    # and they don't get used until the first method is called, so if
-    # they are incorrect, they don't throw until the first method is called.
-    # It's much better to have them throw when $paws->service('...') is called
-    # as this is the point where the user had specified "incorrect" information,
-    # instead of the problem happening in the first method call.
-    $self->endpoint;
-    $self->_region_for_signature;
-  }
-
-
   has region => (is => 'rw', isa => 'Str|Undef');
   requires 'service';
 
--- lib/Paws/Net/V3HTTPSSignature.pm
+++ lib/Paws/Net/V3HTTPSSignature.pm
@@ -5,6 +5,22 @@ package Paws::Net::V3HTTPSSignature;
   requires 'service';
   use POSIX qw(strftime);
 
+  sub BUILD {
+    my $self = shift;
+
+    # These calls are here so that when you construct
+    # the object the endpoint information and the _region_for_signature
+    # are calculated during construction. This is to avoid the fact that 
+    # these attributes are lazy (because they depend on other attributes) 
+    # and they don't get used until the first method is called, so if
+    # they are incorrect, they don't throw until the first method is called.
+    # It's much better to have them throw when $paws->service('...') is called
+    # as this is the point where the user had specified "incorrect" information,
+    # instead of the problem happening in the first method call.
+    $self->endpoint;
+    $self->_region_for_signature;
+  }
+
   sub sign {
     my ($self, $request) = @_;
 
