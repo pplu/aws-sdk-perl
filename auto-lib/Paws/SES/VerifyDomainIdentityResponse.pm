2ea995aecff5cfefd0411a57182c9e3d46710830
Jose Luis Martinez <jlmartinez@capside.com>
2017-08-02 23:38:53 +0200

    API update: SES

--- auto-lib/Paws/SES/VerifyDomainIdentityResponse.pm
+++ auto-lib/Paws/SES/VerifyDomainIdentityResponse.pm
@@ -17,8 +17,15 @@ Paws::SES::VerifyDomainIdentityResponse
 
 =head2 B<REQUIRED> VerificationToken => Str
 
-A TXT record that must be placed in the DNS settings for the domain, in
-order to complete domain verification.
+A TXT record that you must place in the DNS settings of the domain to
+complete domain verification with Amazon SES.
+
+As Amazon SES searches for the TXT record, the domain's verification
+status is "Pending". When Amazon SES detects the record, the domain's
+verification status changes to "Success". If Amazon SES is unable to
+detect the record within 72 hours, the domain's verification status
+changes to "Failed." In that case, if you still want to verify the
+domain, you must restart the verification process from the beginning.
 
 
 =head2 _request_id => Str
