dec77331767452660f29cc19613aeb5f94411b3c
Jose Luis Martinez <jlmartinez@capside.com>
2017-08-25 10:24:33 +0200

    API update: EFS

--- auto-lib/Paws/EFS/CreateFileSystem.pm
+++ auto-lib/Paws/EFS/CreateFileSystem.pm
@@ -2,6 +2,8 @@
 package Paws::EFS::CreateFileSystem;
   use Moose;
   has CreationToken => (is => 'ro', isa => 'Str', required => 1);
+  has Encrypted => (is => 'ro', isa => 'Bool');
+  has KmsKeyId => (is => 'ro', isa => 'Str');
   has PerformanceMode => (is => 'ro', isa => 'Str');
 
   use MooseX::ClassAttribute;
@@ -40,6 +42,53 @@ Values for attributes that are native types (Int, String, Float, etc) can passed
 
 String of up to 64 ASCII characters. Amazon EFS uses this to ensure
 idempotent creation.
+
+
+
+=head2 Encrypted => Bool
+
+A boolean value that, if true, creates an encrypted file system. When
+creating an encrypted file system, you have the option of specifying a
+CreateFileSystemRequest$KmsKeyId for an existing AWS Key Management
+Service (AWS KMS) customer master key (CMK). If you don't specify a
+CMK, then the default CMK for Amazon EFS, C</aws/elasticfilesystem>, is
+used to protect the encrypted file system.
+
+
+
+=head2 KmsKeyId => Str
+
+The id of the AWS KMS CMK that will be used to protect the encrypted
+file system. This parameter is only required if you want to use a
+non-default CMK. If this parameter is not specified, the default CMK
+for Amazon EFS is used. This id can be in one of the following formats:
+
+=over
+
+=item *
+
+Key ID - A unique identifier of the key. For example,
+C<1234abcd-12ab-34cd-56ef-1234567890ab>.
+
+=item *
+
+ARN - An Amazon Resource Name for the key. For example,
+C<arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab>.
+
+=item *
+
+Key alias - A previously created display name for a key. For example,
+C<alias/projectKey1>.
+
+=item *
+
+Key alias ARN - An Amazon Resource Name for a key alias. For example,
+C<arn:aws:kms:us-west-2:444455556666:alias/projectKey1>.
+
+=back
+
+Note that if the KmsKeyId is specified, the
+CreateFileSystemRequest$Encrypted parameter must be set to true.
 
 
 
