2d4da4ada6ffe83ceea6ae593e90bbbc799a8932
Jose Luis Martinez <jlmartinez@capside.com>
2017-09-08 20:31:01 +0000

    ApplicationAutoscaling docu update

--- auto-lib/Paws/ApplicationAutoScaling/TargetTrackingScalingPolicyConfiguration.pm
+++ auto-lib/Paws/ApplicationAutoScaling/TargetTrackingScalingPolicyConfiguration.pm
@@ -48,7 +48,11 @@ Represents a target tracking scaling policy configuration.
 
 =head2 DisableScaleIn => Bool
 
-  
+  Indicates whether scale in by the target tracking policy is disabled.
+If the value is C<true>, scale in is disabled and the target tracking
+policy won't remove capacity from the scalable resource. Otherwise,
+scale in is enabled and the target tracking policy can remove capacity
+from the scalable resource. The default value is C<false>.
 
 
 =head2 PredefinedMetricSpecification => L<Paws::ApplicationAutoScaling::PredefinedMetricSpecification>
