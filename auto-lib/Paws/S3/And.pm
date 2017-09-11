7102f77981099593262e2ae3bcbb56d36c20d266
Sven Schubert <sven.schubert@bitgroup.de>
2017-09-11 10:36:54 +0200

    fix wrong declarations

--- auto-lib/Paws/S3/And.pm
+++ auto-lib/Paws/S3/And.pm
@@ -3,8 +3,6 @@ package Paws::S3::And;
   use Moose;
   has Prefix  => (is => 'ro', isa => 'Str');
   has Tag     => (is => 'ro', isa => 'Paws::S3::Tag');
-
-  has _request_id => (is => 'ro', isa => 'Str');
 1;
 
 ### main pod documentation begin ###
--- auto-lib/Paws/S3/Filter.pm
+++ auto-lib/Paws/S3/Filter.pm
@@ -1,10 +1,8 @@
 
 package Paws::S3::Filter;
   use Moose;
-  has And     => (is => 'ro', isa => 'Paws::S3::And', traits => ['NameInRequest'], request_name => 'And');
-  has Prefix  => (is => 'ro', isa => 'Str|Undef',     traits => ['NameInRequest'], request_name => 'Prefix');
-
-  has _request_id => (is => 'ro', isa => 'Str');
+  has And     => (is => 'ro', isa => 'Paws::S3::And');
+  has Prefix  => (is => 'ro', isa => 'Str');
 1;
 
 ### main pod documentation begin ###
@@ -19,7 +17,7 @@ Paws::S3::Filter
 =head2 And => L<Paws::S3::And>
 
 
-=head2 Prefix => Str|Undef
+=head2 Prefix => Str
 
 
 
