21b24f2adf5d94f56f1127c6f99010e61488f91a
Jose Luis Martinez <jlmartinez@capside.com>
2017-12-09 22:57:10 +0000

    regenerate medialive with updated model

--- auto-lib/Paws/MediaLive/CreateChannel.pm
+++ auto-lib/Paws/MediaLive/CreateChannel.pm
@@ -1,88 +1,93 @@
+
 package Paws::MediaLive::CreateChannel;
   use Moose;
-  has Destinations => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::OutputDestination]', request_name => 'destinations', traits => ['NameInRequest']);
-  has EncoderSettings => (is => 'ro', isa => 'Paws::MediaLive::EncoderSettings', request_name => 'encoderSettings', traits => ['NameInRequest']);
-  has InputAttachments => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::InputAttachment]', request_name => 'inputAttachments', traits => ['NameInRequest']);
-  has Name => (is => 'ro', isa => 'Str', request_name => 'name', traits => ['NameInRequest']);
-  has RequestId => (is => 'ro', isa => 'Str', request_name => 'requestId', traits => ['NameInRequest']);
-  has Reserved => (is => 'ro', isa => 'Str', request_name => 'reserved', traits => ['NameInRequest']);
-  has RoleArn => (is => 'ro', isa => 'Str', request_name => 'roleArn', traits => ['NameInRequest']);
+  has Destinations => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::OutputDestination]', traits => ['NameInRequest'], request_name => 'destinations');
+  has EncoderSettings => (is => 'ro', isa => 'Paws::MediaLive::EncoderSettings', traits => ['NameInRequest'], request_name => 'encoderSettings');
+  has InputAttachments => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::InputAttachment]', traits => ['NameInRequest'], request_name => 'inputAttachments');
+  has Name => (is => 'ro', isa => 'Str', traits => ['NameInRequest'], request_name => 'name');
+  has RequestId => (is => 'ro', isa => 'Str', traits => ['NameInRequest'], request_name => 'requestId');
+  has Reserved => (is => 'ro', isa => 'Str', traits => ['NameInRequest'], request_name => 'reserved');
+  has RoleArn => (is => 'ro', isa => 'Str', traits => ['NameInRequest'], request_name => 'roleArn');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'CreateChannel');
+  class_has _api_uri  => (isa => 'Str', is => 'ro', default => '/prod/channels');
+  class_has _api_method  => (isa => 'Str', is => 'ro', default => 'POST');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Paws::MediaLive::CreateChannelResponse');
 1;
 
 ### main pod documentation begin ###
 
 =head1 NAME
 
-Paws::MediaLive::CreateChannel
-
-=head1 USAGE
-
-This class represents one of two things:
-
-=head3 Arguments in a call to a service
-
-Use the attributes of this class as arguments to methods. You shouldn't make instances of this class. 
-Each attribute should be used as a named argument in the calls that expect this type of object.
-
-As an example, if Att1 is expected to be a Paws::MediaLive::CreateChannel object:
-
-  $service_obj->Method(Att1 => { Destinations => $value, ..., RoleArn => $value  });
-
-=head3 Results returned from an API call
-
-Use accessors for each attribute. If Att1 is expected to be an Paws::MediaLive::CreateChannel object:
-
-  $result = $service_obj->Method(...);
-  $result->Att1->Destinations
+Paws::MediaLive::CreateChannel - Arguments for method CreateChannel on L<Paws::MediaLive>
 
 =head1 DESCRIPTION
 
-Placeholder documentation for CreateChannel
+This class represents the parameters used for calling the method CreateChannel on the 
+AWS Elemental MediaLive service. Use the attributes of this class
+as arguments to method CreateChannel.
+
+You shouldn't make instances of this class. Each attribute should be used as a named argument in the call to CreateChannel.
+
+As an example:
+
+  $service_obj->CreateChannel(Att1 => $value1, Att2 => $value2, ...);
+
+Values for attributes that are native types (Int, String, Float, etc) can passed as-is (scalar values). Values for complex Types (objects) can be passed as a HashRef. The keys and values of the hashref will be used to instance the underlying object.
 
 =head1 ATTRIBUTES
 
 
 =head2 Destinations => ArrayRef[L<Paws::MediaLive::OutputDestination>]
 
-  
+
+
 
 
 =head2 EncoderSettings => L<Paws::MediaLive::EncoderSettings>
 
-  
+
+
 
 
 =head2 InputAttachments => ArrayRef[L<Paws::MediaLive::InputAttachment>]
 
-  List of input attachments for channel.
+List of input attachments for channel.
+
 
 
 =head2 Name => Str
 
-  Name of channel.
+Name of channel.
+
 
 
 =head2 RequestId => Str
 
-  Unique request ID to be specified. This is needed to prevent retries
+Unique request ID to be specified. This is needed to prevent retries
 from creating multiple resources.
+
 
 
 =head2 Reserved => Str
 
-  Reserved for future use.
+Reserved for future use.
+
 
 
 =head2 RoleArn => Str
 
-  An optional Amazon Resource Name (ARN) of the role to assume when
+An optional Amazon Resource Name (ARN) of the role to assume when
 running the Channel.
+
 
 
 
 =head1 SEE ALSO
 
-This class forms part of L<Paws>, describing an object used in L<Paws::MediaLive>
+This class forms part of L<Paws>, documenting arguments for method CreateChannel in L<Paws::MediaLive>
 
 =head1 BUGS and CONTRIBUTIONS
 
--- auto-lib/Paws/MediaLive/CreateInput.pm
+++ auto-lib/Paws/MediaLive/CreateInput.pm
@@ -1,84 +1,88 @@
+
 package Paws::MediaLive::CreateInput;
   use Moose;
-  has Destinations => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::InputDestinationRequest]', request_name => 'destinations', traits => ['NameInRequest']);
-  has InputSecurityGroups => (is => 'ro', isa => 'ArrayRef[Str|Undef]', request_name => 'inputSecurityGroups', traits => ['NameInRequest']);
-  has Name => (is => 'ro', isa => 'Str', request_name => 'name', traits => ['NameInRequest']);
-  has RequestId => (is => 'ro', isa => 'Str', request_name => 'requestId', traits => ['NameInRequest']);
-  has Sources => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::InputSourceRequest]', request_name => 'sources', traits => ['NameInRequest']);
-  has Type => (is => 'ro', isa => 'Str', request_name => 'type', traits => ['NameInRequest']);
+  has Destinations => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::InputDestinationRequest]', traits => ['NameInRequest'], request_name => 'destinations');
+  has InputSecurityGroups => (is => 'ro', isa => 'ArrayRef[Str|Undef]', traits => ['NameInRequest'], request_name => 'inputSecurityGroups');
+  has Name => (is => 'ro', isa => 'Str', traits => ['NameInRequest'], request_name => 'name');
+  has RequestId => (is => 'ro', isa => 'Str', traits => ['NameInRequest'], request_name => 'requestId');
+  has Sources => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::InputSourceRequest]', traits => ['NameInRequest'], request_name => 'sources');
+  has Type => (is => 'ro', isa => 'Str', traits => ['NameInRequest'], request_name => 'type');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'CreateInput');
+  class_has _api_uri  => (isa => 'Str', is => 'ro', default => '/prod/inputs');
+  class_has _api_method  => (isa => 'Str', is => 'ro', default => 'POST');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Paws::MediaLive::CreateInputResponse');
 1;
 
 ### main pod documentation begin ###
 
 =head1 NAME
 
-Paws::MediaLive::CreateInput
-
-=head1 USAGE
-
-This class represents one of two things:
-
-=head3 Arguments in a call to a service
-
-Use the attributes of this class as arguments to methods. You shouldn't make instances of this class. 
-Each attribute should be used as a named argument in the calls that expect this type of object.
-
-As an example, if Att1 is expected to be a Paws::MediaLive::CreateInput object:
-
-  $service_obj->Method(Att1 => { Destinations => $value, ..., Type => $value  });
-
-=head3 Results returned from an API call
-
-Use accessors for each attribute. If Att1 is expected to be an Paws::MediaLive::CreateInput object:
-
-  $result = $service_obj->Method(...);
-  $result->Att1->Destinations
+Paws::MediaLive::CreateInput - Arguments for method CreateInput on L<Paws::MediaLive>
 
 =head1 DESCRIPTION
 
-Placeholder documentation for CreateInput
+This class represents the parameters used for calling the method CreateInput on the 
+AWS Elemental MediaLive service. Use the attributes of this class
+as arguments to method CreateInput.
+
+You shouldn't make instances of this class. Each attribute should be used as a named argument in the call to CreateInput.
+
+As an example:
+
+  $service_obj->CreateInput(Att1 => $value1, Att2 => $value2, ...);
+
+Values for attributes that are native types (Int, String, Float, etc) can passed as-is (scalar values). Values for complex Types (objects) can be passed as a HashRef. The keys and values of the hashref will be used to instance the underlying object.
 
 =head1 ATTRIBUTES
 
 
 =head2 Destinations => ArrayRef[L<Paws::MediaLive::InputDestinationRequest>]
 
-  settings required for PUSH-type inputs; one per redundancy group. Only
+settings required for PUSH-type inputs; one per redundancy group. Only
 one of sources and destinations can be specified. Note: there are
 currently no settings required for PUSH-type inputs
 
 
+
 =head2 InputSecurityGroups => ArrayRef[Str|Undef]
 
-  A list of security groups referenced by IDs to attach to the input.
+A list of security groups referenced by IDs to attach to the input.
+
 
 
 =head2 Name => Str
 
-  Name of the input.
+Name of the input.
+
 
 
 =head2 RequestId => Str
 
-  Unique identifier of the request to ensure the request is handled
+Unique identifier of the request to ensure the request is handled
 exactly once in case of retries
+
 
 
 =head2 Sources => ArrayRef[L<Paws::MediaLive::InputSourceRequest>]
 
-  settings required for PULL-type inputs; one per redundancy group Only
+settings required for PULL-type inputs; one per redundancy group Only
 one of sources and destinations can be specified
+
 
 
 =head2 Type => Str
 
-  
 
+
+Valid values are: C<"UDP_PUSH">, C<"RTP_PUSH">, C<"RTMP_PUSH">, C<"RTMP_PULL">, C<"URL_PULL">
 
 
 =head1 SEE ALSO
 
-This class forms part of L<Paws>, describing an object used in L<Paws::MediaLive>
+This class forms part of L<Paws>, documenting arguments for method CreateInput in L<Paws::MediaLive>
 
 =head1 BUGS and CONTRIBUTIONS
 
