b68de7d102f2141be59914d3bf6fbc1a58808a11
Jose Luis Martinez <jlmartinez@capside.com>
2018-02-19 22:41:17 +0100

    Correction to MediaLive API

--- auto-lib/Paws/MediaLive/UpdateChannel.pm
+++ auto-lib/Paws/MediaLive/UpdateChannel.pm
@@ -1,77 +1,87 @@
+
 package Paws::MediaLive::UpdateChannel;
   use Moose;
-  has Destinations => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::OutputDestination]', request_name => 'destinations', traits => ['NameInRequest']);
-  has EncoderSettings => (is => 'ro', isa => 'Paws::MediaLive::EncoderSettings', request_name => 'encoderSettings', traits => ['NameInRequest']);
-  has InputSpecification => (is => 'ro', isa => 'Paws::MediaLive::InputSpecification', request_name => 'inputSpecification', traits => ['NameInRequest']);
-  has Name => (is => 'ro', isa => 'Str', request_name => 'name', traits => ['NameInRequest']);
-  has RoleArn => (is => 'ro', isa => 'Str', request_name => 'roleArn', traits => ['NameInRequest']);
+  has ChannelId => (is => 'ro', isa => 'Str', traits => ['ParamInURI'], uri_name => 'channelId', required => 1);
+  has Destinations => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::OutputDestination]', traits => ['NameInRequest'], request_name => 'destinations');
+  has EncoderSettings => (is => 'ro', isa => 'Paws::MediaLive::EncoderSettings', traits => ['NameInRequest'], request_name => 'encoderSettings');
+  has InputSpecification => (is => 'ro', isa => 'Paws::MediaLive::InputSpecification', traits => ['NameInRequest'], request_name => 'inputSpecification');
+  has Name => (is => 'ro', isa => 'Str', traits => ['NameInRequest'], request_name => 'name');
+  has RoleArn => (is => 'ro', isa => 'Str', traits => ['NameInRequest'], request_name => 'roleArn');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'UpdateChannel');
+  class_has _api_uri  => (isa => 'Str', is => 'ro', default => '/prod/channels/{channelId}');
+  class_has _api_method  => (isa => 'Str', is => 'ro', default => 'PUT');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Paws::MediaLive::UpdateChannelResponse');
 1;
 
 ### main pod documentation begin ###
 
 =head1 NAME
 
-Paws::MediaLive::UpdateChannel
-
-=head1 USAGE
-
-This class represents one of two things:
-
-=head3 Arguments in a call to a service
-
-Use the attributes of this class as arguments to methods. You shouldn't make instances of this class. 
-Each attribute should be used as a named argument in the calls that expect this type of object.
-
-As an example, if Att1 is expected to be a Paws::MediaLive::UpdateChannel object:
-
-  $service_obj->Method(Att1 => { Destinations => $value, ..., RoleArn => $value  });
-
-=head3 Results returned from an API call
-
-Use accessors for each attribute. If Att1 is expected to be an Paws::MediaLive::UpdateChannel object:
-
-  $result = $service_obj->Method(...);
-  $result->Att1->Destinations
+Paws::MediaLive::UpdateChannel - Arguments for method UpdateChannel on L<Paws::MediaLive>
 
 =head1 DESCRIPTION
 
-Placeholder documentation for UpdateChannel
+This class represents the parameters used for calling the method UpdateChannel on the 
+AWS Elemental MediaLive service. Use the attributes of this class
+as arguments to method UpdateChannel.
+
+You shouldn't make instances of this class. Each attribute should be used as a named argument in the call to UpdateChannel.
+
+As an example:
+
+  $service_obj->UpdateChannel(Att1 => $value1, Att2 => $value2, ...);
+
+Values for attributes that are native types (Int, String, Float, etc) can passed as-is (scalar values). Values for complex Types (objects) can be passed as a HashRef. The keys and values of the hashref will be used to instance the underlying object.
 
 =head1 ATTRIBUTES
 
 
+=head2 B<REQUIRED> ChannelId => Str
+
+channel ID
+
+
+
 =head2 Destinations => ArrayRef[L<Paws::MediaLive::OutputDestination>]
 
-  A list of output destinations for this channel.
+A list of output destinations for this channel.
+
 
 
 =head2 EncoderSettings => L<Paws::MediaLive::EncoderSettings>
 
-  The encoder settings for this channel.
+The encoder settings for this channel.
+
 
 
 =head2 InputSpecification => L<Paws::MediaLive::InputSpecification>
 
-  Specification of input for this channel (max. bitrate, resolution,
+Specification of input for this channel (max. bitrate, resolution,
 codec, etc.)
+
 
 
 =head2 Name => Str
 
-  The name of the channel.
+The name of the channel.
+
 
 
 =head2 RoleArn => Str
 
-  An optional Amazon Resource Name (ARN) of the role to assume when
+An optional Amazon Resource Name (ARN) of the role to assume when
 running the Channel. If you do not specify this on an update call but
 the role was previously set that role will be removed.
 
 
 
+
 =head1 SEE ALSO
 
-This class forms part of L<Paws>, describing an object used in L<Paws::MediaLive>
+This class forms part of L<Paws>, documenting arguments for method UpdateChannel in L<Paws::MediaLive>
 
 =head1 BUGS and CONTRIBUTIONS
 
