6d632078d50ba0e75ae430e99ded2300fdc50b2d
Jose Luis Martinez <jlmartinez@capside.com>
2018-04-04 21:10:26 +0200

    Regenerate MediaLive with updated boto spec

--- auto-lib/Paws/MediaLive/UpdateInput.pm
+++ auto-lib/Paws/MediaLive/UpdateInput.pm
@@ -1,70 +1,79 @@
+
 package Paws::MediaLive::UpdateInput;
   use Moose;
-  has Destinations => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::InputDestinationRequest]', request_name => 'destinations', traits => ['NameInRequest']);
-  has InputSecurityGroups => (is => 'ro', isa => 'ArrayRef[Str|Undef]', request_name => 'inputSecurityGroups', traits => ['NameInRequest']);
-  has Name => (is => 'ro', isa => 'Str', request_name => 'name', traits => ['NameInRequest']);
-  has Sources => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::InputSourceRequest]', request_name => 'sources', traits => ['NameInRequest']);
+  has Destinations => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::InputDestinationRequest]', traits => ['NameInRequest'], request_name => 'destinations');
+  has InputId => (is => 'ro', isa => 'Str', traits => ['ParamInURI'], uri_name => 'inputId', required => 1);
+  has InputSecurityGroups => (is => 'ro', isa => 'ArrayRef[Str|Undef]', traits => ['NameInRequest'], request_name => 'inputSecurityGroups');
+  has Name => (is => 'ro', isa => 'Str', traits => ['NameInRequest'], request_name => 'name');
+  has Sources => (is => 'ro', isa => 'ArrayRef[Paws::MediaLive::InputSourceRequest]', traits => ['NameInRequest'], request_name => 'sources');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'UpdateInput');
+  class_has _api_uri  => (isa => 'Str', is => 'ro', default => '/prod/inputs/{inputId}');
+  class_has _api_method  => (isa => 'Str', is => 'ro', default => 'PUT');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Paws::MediaLive::UpdateInputResponse');
 1;
 
 ### main pod documentation begin ###
 
 =head1 NAME
 
-Paws::MediaLive::UpdateInput
-
-=head1 USAGE
-
-This class represents one of two things:
-
-=head3 Arguments in a call to a service
-
-Use the attributes of this class as arguments to methods. You shouldn't make instances of this class. 
-Each attribute should be used as a named argument in the calls that expect this type of object.
-
-As an example, if Att1 is expected to be a Paws::MediaLive::UpdateInput object:
-
-  $service_obj->Method(Att1 => { Destinations => $value, ..., Sources => $value  });
-
-=head3 Results returned from an API call
-
-Use accessors for each attribute. If Att1 is expected to be an Paws::MediaLive::UpdateInput object:
-
-  $result = $service_obj->Method(...);
-  $result->Att1->Destinations
+Paws::MediaLive::UpdateInput - Arguments for method UpdateInput on L<Paws::MediaLive>
 
 =head1 DESCRIPTION
 
-Placeholder documentation for UpdateInput
+This class represents the parameters used for calling the method UpdateInput on the 
+AWS Elemental MediaLive service. Use the attributes of this class
+as arguments to method UpdateInput.
+
+You shouldn't make instances of this class. Each attribute should be used as a named argument in the call to UpdateInput.
+
+As an example:
+
+  $service_obj->UpdateInput(Att1 => $value1, Att2 => $value2, ...);
+
+Values for attributes that are native types (Int, String, Float, etc) can passed as-is (scalar values). Values for complex Types (objects) can be passed as a HashRef. The keys and values of the hashref will be used to instance the underlying object.
 
 =head1 ATTRIBUTES
 
 
 =head2 Destinations => ArrayRef[L<Paws::MediaLive::InputDestinationRequest>]
 
-  Destination settings for PUSH type inputs.
+Destination settings for PUSH type inputs.
+
+
+
+=head2 B<REQUIRED> InputId => Str
+
+Unique ID of the input.
+
 
 
 =head2 InputSecurityGroups => ArrayRef[Str|Undef]
 
-  A list of security groups referenced by IDs to attach to the input.
+A list of security groups referenced by IDs to attach to the input.
+
 
 
 =head2 Name => Str
 
-  Name of the input.
+Name of the input.
+
 
 
 =head2 Sources => ArrayRef[L<Paws::MediaLive::InputSourceRequest>]
 
-  The source URLs for a PULL-type input. Every PULL type input needs
+The source URLs for a PULL-type input. Every PULL type input needs
 exactly two source URLs for redundancy. Only specify sources for PULL
 type Inputs. Leave Destinations empty.
 
 
 
+
 =head1 SEE ALSO
 
-This class forms part of L<Paws>, describing an object used in L<Paws::MediaLive>
+This class forms part of L<Paws>, documenting arguments for method UpdateInput in L<Paws::MediaLive>
 
 =head1 BUGS and CONTRIBUTIONS
 
