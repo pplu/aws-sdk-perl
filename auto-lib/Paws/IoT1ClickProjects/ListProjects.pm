ab57f545c8de9182bb452767ca496b6c424499ba
Jose Luis Martinez <jlmartinez@capside.com>
2018-05-15 21:04:01 +0200

    New API IoT1ClickProjects

---
+++ auto-lib/Paws/IoT1ClickProjects/ListProjects.pm
@@ -0,0 +1,63 @@
+
+package Paws::IoT1ClickProjects::ListProjects;
+  use Moose;
+  has MaxResults => (is => 'ro', isa => 'Int', traits => ['ParamInQuery'], query_name => 'maxResults');
+  has NextToken => (is => 'ro', isa => 'Str', traits => ['ParamInQuery'], query_name => 'nextToken');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'ListProjects');
+  class_has _api_uri  => (isa => 'Str', is => 'ro', default => '/projects');
+  class_has _api_method  => (isa => 'Str', is => 'ro', default => 'GET');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Paws::IoT1ClickProjects::ListProjectsResponse');
+1;
+
+### main pod documentation begin ###
+
+=head1 NAME
+
+Paws::IoT1ClickProjects::ListProjects - Arguments for method ListProjects on L<Paws::IoT1ClickProjects>
+
+=head1 DESCRIPTION
+
+This class represents the parameters used for calling the method ListProjects on the 
+L<AWS IoT 1-Click Projects Service|Paws::IoT1ClickProjects> service. Use the attributes of this class
+as arguments to method ListProjects.
+
+You shouldn't make instances of this class. Each attribute should be used as a named argument in the call to ListProjects.
+
+As an example:
+
+  $service_obj->ListProjects(Att1 => $value1, Att2 => $value2, ...);
+
+Values for attributes that are native types (Int, String, Float, etc) can passed as-is (scalar values). Values for complex Types (objects) can be passed as a HashRef. The keys and values of the hashref will be used to instance the underlying object.
+
+=head1 ATTRIBUTES
+
+
+=head2 MaxResults => Int
+
+The maximum number of results to return per request. If not set, a
+default value of 100 is used.
+
+
+
+=head2 NextToken => Str
+
+The token to retrieve the next set of results.
+
+
+
+
+=head1 SEE ALSO
+
+This class forms part of L<Paws>, documenting arguments for method ListProjects in L<Paws::IoT1ClickProjects>
+
+=head1 BUGS and CONTRIBUTIONS
+
+The source code is located here: L<https://github.com/pplu/aws-sdk-perl>
+
+Please report bugs to: L<https://github.com/pplu/aws-sdk-perl/issues>
+
+=cut
+
