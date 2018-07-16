d314c13b9c85583d46dc376db4d7b8027e59d5d6
Jose Luis Martinez <jlmartinez@capside.com>
2018-07-16 11:15:00 +0000

    Regenerate MediaLive with new definition

--- auto-lib/Paws/MediaLive/CreateChannel.pm
+++ auto-lib/Paws/MediaLive/CreateChannel.pm
@@ -198,8 +198,7 @@ You shouldn't make instances of this class. Each attribute should be used as a n
                 ClientCache => 'DISABLED', # values: DISABLED, ENABLED; OPTIONAL
                 CodecSpecification =>
                   'RFC_4281',    # values: RFC_4281, RFC_6381; OPTIONAL
-                ConstantIv =>
-                  'My__stringMin32Max32',    # min: 32, max: 32; OPTIONAL
+                ConstantIv         => 'My__stringMin32Max32', # min: 32, max: 32
                 DirectoryStructure => 'SINGLE_DIRECTORY'
                 ,  # values: SINGLE_DIRECTORY, SUBDIRECTORY_PER_STREAM; OPTIONAL
                 EncryptionType =>
@@ -249,12 +248,11 @@ You shouldn't make instances of this class. Each attribute should be used as a n
                 KeyFormatVersions   => 'My__string',    # OPTIONAL
                 KeyProviderSettings => {
                   StaticKeySettings => {
-                    StaticKeyValue =>
-                      'My__stringMin32Max32',    # min: 32, max: 32; OPTIONAL
+                    StaticKeyValue => 'My__stringMin32Max32', # min: 32, max: 32
                     KeyProviderServer => {
-                      Uri           => 'My__string',    # OPTIONAL
-                      PasswordParam => 'My__string',    # OPTIONAL
-                      Username      => 'My__string',    # OPTIONAL
+                      Uri           => 'My__string',          # OPTIONAL
+                      PasswordParam => 'My__string',          # OPTIONAL
+                      Username      => 'My__string',          # OPTIONAL
                     },    # OPTIONAL
                   },    # OPTIONAL
                 },    # OPTIONAL
@@ -355,7 +353,7 @@ You shouldn't make instances of this class. Each attribute should be used as a n
                         DvbNitSettings => {
                           NetworkId => 1,    # max: 65536
                           NetworkName =>
-                            'My__stringMin1Max256', # min: 1, max: 256; OPTIONAL
+                            'My__stringMin1Max256',    # min: 1, max: 256
                           RepInterval => 1,    # min: 25, max: 10000; OPTIONAL
                         },    # OPTIONAL
                         DvbSdtSettings => {
@@ -363,9 +361,9 @@ You shouldn't make instances of this class. Each attribute should be used as a n
                           , # values: SDT_FOLLOW, SDT_FOLLOW_IF_PRESENT, SDT_MANUAL, SDT_NONE; OPTIONAL
                           RepInterval => 1,    # min: 25, max: 2000; OPTIONAL
                           ServiceName =>
-                            'My__stringMin1Max256', # min: 1, max: 256; OPTIONAL
+                            'My__stringMin1Max256',    # min: 1, max: 256
                           ServiceProviderName =>
-                            'My__stringMin1Max256', # min: 1, max: 256; OPTIONAL
+                            'My__stringMin1Max256',    # min: 1, max: 256
                         },    # OPTIONAL
                         DvbSubPids     => 'My__string',    # OPTIONAL
                         DvbTdtSettings => {
@@ -490,7 +488,7 @@ You shouldn't make instances of this class. Each attribute should be used as a n
                         DvbNitSettings => {
                           NetworkId => 1,    # max: 65536
                           NetworkName =>
-                            'My__stringMin1Max256', # min: 1, max: 256; OPTIONAL
+                            'My__stringMin1Max256',    # min: 1, max: 256
                           RepInterval => 1,    # min: 25, max: 10000; OPTIONAL
                         },    # OPTIONAL
                         DvbSdtSettings => {
@@ -498,9 +496,9 @@ You shouldn't make instances of this class. Each attribute should be used as a n
                           , # values: SDT_FOLLOW, SDT_FOLLOW_IF_PRESENT, SDT_MANUAL, SDT_NONE; OPTIONAL
                           RepInterval => 1,    # min: 25, max: 2000; OPTIONAL
                           ServiceName =>
-                            'My__stringMin1Max256', # min: 1, max: 256; OPTIONAL
+                            'My__stringMin1Max256',    # min: 1, max: 256
                           ServiceProviderName =>
-                            'My__stringMin1Max256', # min: 1, max: 256; OPTIONAL
+                            'My__stringMin1Max256',    # min: 1, max: 256
                         },    # OPTIONAL
                         DvbSubPids     => 'My__string',    # OPTIONAL
                         DvbTdtSettings => {
--- auto-lib/Paws/MediaLive/PurchaseOffering.pm
+++ auto-lib/Paws/MediaLive/PurchaseOffering.pm
@@ -1,63 +1,83 @@
+
 package Paws::MediaLive::PurchaseOffering;
   use Moose;
-  has Count => (is => 'ro', isa => 'Int', request_name => 'count', traits => ['NameInRequest']);
-  has Name => (is => 'ro', isa => 'Str', request_name => 'name', traits => ['NameInRequest']);
-  has RequestId => (is => 'ro', isa => 'Str', request_name => 'requestId', traits => ['NameInRequest']);
+  has Count => (is => 'ro', isa => 'Int', traits => ['NameInRequest'], request_name => 'count');
+  has Name => (is => 'ro', isa => 'Str', traits => ['NameInRequest'], request_name => 'name');
+  has OfferingId => (is => 'ro', isa => 'Str', traits => ['ParamInURI'], uri_name => 'offeringId', required => 1);
+  has RequestId => (is => 'ro', isa => 'Str', traits => ['NameInRequest'], request_name => 'requestId');
+
+  use MooseX::ClassAttribute;
+
+  class_has _api_call => (isa => 'Str', is => 'ro', default => 'PurchaseOffering');
+  class_has _api_uri  => (isa => 'Str', is => 'ro', default => '/prod/offerings/{offeringId}/purchase');
+  class_has _api_method  => (isa => 'Str', is => 'ro', default => 'POST');
+  class_has _returns => (isa => 'Str', is => 'ro', default => 'Paws::MediaLive::PurchaseOfferingResponse');
 1;
 
 ### main pod documentation begin ###
 
 =head1 NAME
 
-Paws::MediaLive::PurchaseOffering
-
-=head1 USAGE
-
-This class represents one of two things:
-
-=head3 Arguments in a call to a service
-
-Use the attributes of this class as arguments to methods. You shouldn't make instances of this class. 
-Each attribute should be used as a named argument in the calls that expect this type of object.
-
-As an example, if Att1 is expected to be a Paws::MediaLive::PurchaseOffering object:
-
-  $service_obj->Method(Att1 => { Count => $value, ..., RequestId => $value  });
-
-=head3 Results returned from an API call
-
-Use accessors for each attribute. If Att1 is expected to be an Paws::MediaLive::PurchaseOffering object:
-
-  $result = $service_obj->Method(...);
-  $result->Att1->Count
+Paws::MediaLive::PurchaseOffering - Arguments for method PurchaseOffering on L<Paws::MediaLive>
 
 =head1 DESCRIPTION
 
-PurchaseOffering request
+This class represents the parameters used for calling the method PurchaseOffering on the
+L<AWS Elemental MediaLive|Paws::MediaLive> service. Use the attributes of this class
+as arguments to method PurchaseOffering.
+
+You shouldn't make instances of this class. Each attribute should be used as a named argument in the call to PurchaseOffering.
+
+=head1 SYNOPSIS
+
+    my $medialive = Paws->service('MediaLive');
+    my $PurchaseOfferingResponse = $medialive->PurchaseOffering(
+      OfferingId => 'My__string',
+      Count      => 1,               # OPTIONAL
+      Name       => 'My__string',    # OPTIONAL
+      RequestId  => 'My__string',    # OPTIONAL
+    );
+
+    # Results:
+    my $Reservation = $PurchaseOfferingResponse->Reservation;
+
+    # Returns a L<Paws::MediaLive::PurchaseOfferingResponse> object.
+
+Values for attributes that are native types (Int, String, Float, etc) can passed as-is (scalar values). Values for complex Types (objects) can be passed as a HashRef. The keys and values of the hashref will be used to instance the underlying object.
+For the AWS API documentation, see L<https://docs.aws.amazon.com/goto/WebAPI/medialive/PurchaseOffering>
 
 =head1 ATTRIBUTES
 
 
 =head2 Count => Int
 
-  Number of resources
+Number of resources
+
 
 
 =head2 Name => Str
 
-  Name for the new reservation
+Name for the new reservation
+
+
+
+=head2 B<REQUIRED> OfferingId => Str
+
+Offering to purchase, e.g. '87654321'
+
 
 
 =head2 RequestId => Str
 
-  Unique request ID to be specified. This is needed to prevent retries
+Unique request ID to be specified. This is needed to prevent retries
 from creating multiple resources.
+
 
 
 
 =head1 SEE ALSO
 
-This class forms part of L<Paws>, describing an object used in L<Paws::MediaLive>
+This class forms part of L<Paws>, documenting arguments for method PurchaseOffering in L<Paws::MediaLive>
 
 =head1 BUGS and CONTRIBUTIONS
 
