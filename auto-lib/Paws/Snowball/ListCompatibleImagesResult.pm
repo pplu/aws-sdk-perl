29562e34b02c650303acb9c026959ec9f98a5847
Jose Luis Martinez <jlmartinez@capside.com>
2018-07-30 08:59:05 +0000

    Snowball API update

---
+++ auto-lib/Paws/Snowball/ListCompatibleImagesResult.pm
@@ -0,0 +1,35 @@
+
+package Paws::Snowball::ListCompatibleImagesResult;
+  use Moose;
+  has CompatibleImages => (is => 'ro', isa => 'ArrayRef[Paws::Snowball::CompatibleImage]');
+  has NextToken => (is => 'ro', isa => 'Str');
+
+  has _request_id => (is => 'ro', isa => 'Str');
+
+### main pod documentation begin ###
+
+=head1 NAME
+
+Paws::Snowball::ListCompatibleImagesResult
+
+=head1 ATTRIBUTES
+
+
+=head2 CompatibleImages => ArrayRef[L<Paws::Snowball::CompatibleImage>]
+
+A JSON-formatted object that describes a compatible AMI, including the
+ID and name for a Snowball Edge AMI.
+
+
+=head2 NextToken => Str
+
+Because HTTP requests are stateless, this is the starting point for
+your next list of returned images.
+
+
+=head2 _request_id => Str
+
+
+=cut
+
+1;
