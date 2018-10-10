1dc1f5d28b4540deeaae3bee4b2cb37d0ca3107f
Jose Luis Martinez <jlmartinez@capside.com>
2018-10-10 22:35:19 +0200

    Organizations docu update

--- auto-lib/Paws/Organizations/CreateAccountResponse.pm
+++ auto-lib/Paws/Organizations/CreateAccountResponse.pm
@@ -19,9 +19,13 @@ Paws::Organizations::CreateAccountResponse
 A structure that contains details about the request to create an
 account. This response structure might not be fully populated when you
 first receive it because account creation is an asynchronous process.
-You can pass the returned CreateAccountStatus ID as a parameter to C<
-DescribeCreateAccountStatus > to get status about the progress of the
-request at later times.
+You can pass the returned C<CreateAccountStatus> ID as a parameter to
+DescribeCreateAccountStatus to get status about the progress of the
+request at later times. You can also check the AWS CloudTrail log for
+the C<CreateAccountResult> event. For more information, see Monitoring
+the Activity in Your Organization
+(http://docs.aws.amazon.com/organizations/latest/userguide/orgs_monitoring.html)
+in the I<AWS Organizations User Guide>.
 
 
 =head2 _request_id => Str
