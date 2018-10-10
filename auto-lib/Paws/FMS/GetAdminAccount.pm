8c0a8705bef1bb4f600dec1ec4e16e86bd1d6dc5
Jose Luis Martinez <jlmartinez@capside.com>
2018-10-10 22:21:17 +0200

    FMS API update

--- auto-lib/Paws/FMS/GetAdminAccount.pm
+++ auto-lib/Paws/FMS/GetAdminAccount.pm
@@ -30,6 +30,7 @@ You shouldn't make instances of this class. Each attribute should be used as a n
 
     # Results:
     my $AdminAccount = $GetAdminAccountResponse->AdminAccount;
+    my $RoleStatus   = $GetAdminAccountResponse->RoleStatus;
 
     # Returns a L<Paws::FMS::GetAdminAccountResponse> object.
 
