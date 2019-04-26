276d6a89b18e4a3ed2671aa0830ced9969a44aa6
Jose Luis Martínez Torres <joseluis.martinez@capside.com>
2019-04-26 17:18:10 +0200

    Bump version to 0.41

--- auto-lib/Paws.pm
+++ auto-lib/Paws.pm
@@ -57,7 +57,7 @@ __PACKAGE__->meta->make_immutable;
 
 package Paws;
 
-our $VERSION = '0.40';
+our $VERSION = '0.41';
 
 use Carp;
 
--- builder-lib/Paws/API/Builder/Paws.pm
+++ builder-lib/Paws/API/Builder/Paws.pm
@@ -9,7 +9,7 @@ package Paws::API::Builder::Paws {
   use Moose;
 
   sub version {
-    '0.40';
+    '0.41';
   }
 
   sub services {
