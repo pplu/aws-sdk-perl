f6fda5227f489a108a97a76fcd6e005c752b48d2
Jess Robinson <j.robinson@shadowcat.co.uk>
2019-10-17 16:53:11 +0000

    Moo conversion for Kinesis builder

--- builder-lib/Paws/API/Builder/Kinesis.pm
+++ builder-lib/Paws/API/Builder/Kinesis.pm
@@ -1,11 +1,11 @@
 package Paws::API::Builder::Kinesis {
-  use Moose;
+  use Moo;
 
   extends 'Paws::API::Builder::json';
 
   # Kinesis is using exception shapes in it's responses
-  override build_exception_shapes => sub {
-    return {}
+  sub build_exception_shapes {
+      return {};
   }
 }
 
