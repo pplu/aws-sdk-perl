ca398009f33d8fba55819ec0a143ab5b7b2a4ab2
Jose Luis Martinez <jlmartinez@capside.com>
2019-11-19 23:07:22 +0100

    SageMakerRuntime API update

--- auto-lib/Paws/SageMakerRuntime/InvokeEndpoint.pm
+++ auto-lib/Paws/SageMakerRuntime/InvokeEndpoint.pm
@@ -6,6 +6,7 @@ package Paws::SageMakerRuntime::InvokeEndpoint;
   has ContentType => (is => 'ro', isa => 'Str', traits => ['ParamInHeader'], header_name => 'Content-Type');
   has CustomAttributes => (is => 'ro', isa => 'Str', traits => ['ParamInHeader'], header_name => 'X-Amzn-SageMaker-Custom-Attributes');
   has EndpointName => (is => 'ro', isa => 'Str', traits => ['ParamInURI'], uri_name => 'EndpointName', required => 1);
+  has TargetModel => (is => 'ro', isa => 'Str', traits => ['ParamInHeader'], header_name => 'X-Amzn-SageMaker-Target-Model');
 
   use MooseX::ClassAttribute;
   class_has _stream_param => (is => 'ro', default => 'Body');
@@ -38,6 +39,7 @@ You shouldn't make instances of this class. Each attribute should be used as a n
       Accept           => 'MyHeader',                    # OPTIONAL
       ContentType      => 'MyHeader',                    # OPTIONAL
       CustomAttributes => 'MyCustomAttributesHeader',    # OPTIONAL
+      TargetModel      => 'MyTargetModelHeader',         # OPTIONAL
     );
 
     # Results:
@@ -69,7 +71,7 @@ the model.
 
 For information about the format of the request body, see Common Data
 FormatsE<mdash>Inference
-(http://docs.aws.amazon.com/sagemaker/latest/dg/cdf-inference.html).
+(https://docs.aws.amazon.com/sagemaker/latest/dg/cdf-inference.html).
 
 
 
@@ -81,7 +83,16 @@ The MIME type of the input data in the request body.
 
 =head2 CustomAttributes => Str
 
-
+Provides additional information about a request for an inference
+submitted to a model hosted at an Amazon SageMaker endpoint. The
+information is an opaque value that is forwarded verbatim. You could
+use this value, for example, to provide an ID that you can use to track
+a request or to provide other metadata that a service endpoint was
+programmed to process. The value must consist of no more than 1024
+visible US-ASCII characters as specified in Section 3.3.6. Field Value
+Components (https://tools.ietf.org/html/rfc7230#section-3.2.6) of the
+Hypertext Transfer Protocol (HTTP/1.1). This feature is currently
+supported in the AWS SDKs but not in the Amazon SageMaker Python SDK.
 
 
 
@@ -89,8 +100,15 @@ The MIME type of the input data in the request body.
 
 The name of the endpoint that you specified when you created the
 endpoint using the CreateEndpoint
-(http://docs.aws.amazon.com/sagemaker/latest/dg/API_CreateEndpoint.html)
+(https://docs.aws.amazon.com/sagemaker/latest/dg/API_CreateEndpoint.html)
 API.
+
+
+
+=head2 TargetModel => Str
+
+Specifies the model to be requested for an inference when invoking a
+multi-model endpoint.
 
 
 
