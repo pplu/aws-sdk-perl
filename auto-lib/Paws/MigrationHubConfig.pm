ef46de2c097ce41f134c63bdd56e908df87b9a6c
Jose Luis Martinez <pplusdomain@gmail.com>
2021-05-23 11:17:03 +0200

    MigrationHubConfig API update

--- auto-lib/Paws/MigrationHubConfig.pm
+++ auto-lib/Paws/MigrationHubConfig.pm
@@ -66,24 +66,33 @@ working with your Migration Hub home region. You can use these APIs to
 determine a home region, as well as to create and work with controls
 that describe the home region.
 
-You can use these APIs within your home region only. If you call these
-APIs from outside your home region, your calls are rejected, except for
-the ability to register your agents and connectors.
+=over
 
-You must call C<GetHomeRegion> at least once before you call any other
-AWS Application Discovery Service and AWS Migration Hub APIs, to obtain
-the account's Migration Hub home region.
+=item *
 
-The C<StartDataCollection> API call in AWS Application Discovery
-Service allows your agents and connectors to begin collecting data that
-flows directly into the home region, and it will prevent you from
-enabling data collection information to be sent outside the home
-region.
+You must make API calls for write actions (create, notify, associate,
+disassociate, import, or put) while in your home region, or a
+C<HomeRegionNotSetException> error is returned.
+
+=item *
+
+API calls for read actions (list, describe, stop, and delete) are
+permitted outside of your home region.
+
+=item *
+
+If you call a write API outside the home region, an
+C<InvalidInputException> is returned.
+
+=item *
+
+You can call C<GetHomeRegion> action to obtain the account's Migration
+Hub home region.
+
+=back
 
 For specific API usage, see the sections that follow in this AWS
 Migration Hub Home Region API reference.
-
-The Migration Hub Home Region APIs do not support AWS Organizations.
 
 For the AWS API documentation, see L<https://docs.aws.amazon.com/goto/WebAPI/migrationhub-config-2019-06-30>
 
@@ -131,8 +140,8 @@ Each argument is described in detail in: L<Paws::MigrationHubConfig::DescribeHom
 
 Returns: a L<Paws::MigrationHubConfig::DescribeHomeRegionControlsResult> instance
 
-This API permits filtering on the C<ControlId>, C<HomeRegion>, and
-C<RegionControlScope> fields.
+This API permits filtering on the C<ControlId> and C<HomeRegion>
+fields.
 
 
 =head2 GetHomeRegion
