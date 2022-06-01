e3f172119fe792edf5ab7ae0fe91e8db9c355723
Jose Luis Martinez <pplusdomain@gmail.com>
2022-06-01 20:31:57 +0200

    Update documentation

--- auto-lib/Paws.pm
+++ auto-lib/Paws.pm
@@ -1251,5 +1251,9 @@ aeruder for contributing
  - Substituting Config::INI for Config::AWS 
  - Parrallelizing and fixing generation inconsistencies of the SDK
 
+dheffx for making ContainerProfile credential provider more robust
+
+0leksii for building support for Instance Metadata Service v2 (IMDSv2)
+
 
 =cut
